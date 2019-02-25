60fb10617b4ea321acf61d4d681fe2d834eab6db
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2019-02-25 11:33:30 +0000

    Stubbing values required by Spring Boot context during integration test phase

--- src/integrationTest/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/TsvStringProviderTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/TsvStringProviderTest.java
@@ -7,11 +7,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.iacasenotificationsapi.Application;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
+@ActiveProfiles("integration")
 public class TsvStringProviderTest {
 
     @Autowired private TsvStringProvider tsvStringProvider;
