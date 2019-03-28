a5aad81e3a17ebad292bbe10383d0a57a34eb170
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2019-03-28 10:32:11 +0000

    RIA-842 Send legal rep hearing requirements direction

---
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/handlers/presubmit/LegalRepresentativeHearingRequirementsDirectionNotifier.java
@@ -0,0 +1,106 @@
+package uk.gov.hmcts.reform.iacasenotificationsapi.domain.handlers.presubmit;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.NotificationSender;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.Direction;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.DirectionTag;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.field.IdValue;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.handlers.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.LegalRepresentativePersonalisationFactory;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.DirectionFinder;
+
+@Component
+public class LegalRepresentativeHearingRequirementsDirectionNotifier implements PreSubmitCallbackHandler<AsylumCase> {
+
+    private final String legalRepresentativeHearingRequirementsDirectionTemplateId;
+    private final LegalRepresentativePersonalisationFactory legalRepresentativePersonalisationFactory;
+    private final DirectionFinder directionFinder;
+    private final NotificationSender notificationSender;
+
+    public LegalRepresentativeHearingRequirementsDirectionNotifier(
+        @Value("${govnotify.template.legalRepresentativeHearingRequirementsDirection}") String legalRepresentativeHearingRequirementsDirectionTemplateId,
+        LegalRepresentativePersonalisationFactory legalRepresentativePersonalisationFactory,
+        DirectionFinder directionFinder,
+        NotificationSender notificationSender
+    ) {
+        requireNonNull(legalRepresentativeHearingRequirementsDirectionTemplateId, "legalRepresentativeHearingRequirementsDirectionTemplateId must not be null");
+
+        this.legalRepresentativeHearingRequirementsDirectionTemplateId = legalRepresentativeHearingRequirementsDirectionTemplateId;
+        this.legalRepresentativePersonalisationFactory = legalRepresentativePersonalisationFactory;
+        this.directionFinder = directionFinder;
+        this.notificationSender = notificationSender;
+    }
+
+    public boolean canHandle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback
+    ) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+               && callback.getEvent() == Event.REQUEST_HEARING_REQUIREMENTS;
+    }
+
+    public PreSubmitCallbackResponse<AsylumCase> handle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback
+    ) {
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        AsylumCase asylumCase =
+            callback
+                .getCaseDetails()
+                .getCaseData();
+
+        String legalRepresentativeEmailAddress =
+            asylumCase
+                .getLegalRepresentativeEmailAddress()
+                .orElseThrow(() -> new IllegalStateException("legalRepresentativeEmailAddress is not present"));
+
+        Direction legalRepresentativeHearingRequirementsDirection =
+            directionFinder
+                .findFirst(asylumCase, DirectionTag.LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS)
+                .orElseThrow(() -> new IllegalStateException("legal representative hearing requirements direction is not present"));
+
+        Map<String, String> personalisation =
+            legalRepresentativePersonalisationFactory
+                .create(asylumCase, legalRepresentativeHearingRequirementsDirection);
+
+        String reference =
+            callback.getCaseDetails().getId()
+            + "_LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS_DIRECTION";
+
+        String notificationId =
+            notificationSender.sendEmail(
+                legalRepresentativeHearingRequirementsDirectionTemplateId,
+                legalRepresentativeEmailAddress,
+                personalisation,
+                reference
+            );
+
+        List<IdValue<String>> notificationsSent =
+            asylumCase
+                .getNotificationsSent()
+                .orElseGet(ArrayList::new);
+
+        notificationsSent.add(new IdValue<>(reference, notificationId));
+
+        asylumCase.setNotificationsSent(notificationsSent);
+
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/handlers/presubmit/LegalRepresentativeHearingRequirementsDirectionNotifierTest.java
@@ -0,0 +1,235 @@
+package uk.gov.hmcts.reform.iacasenotificationsapi.domain.handlers.presubmit;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.NotificationSender;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.Direction;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.DirectionTag;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.CaseDetails;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.field.IdValue;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.LegalRepresentativePersonalisationFactory;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.DirectionFinder;
+
+@RunWith(MockitoJUnitRunner.class)
+@SuppressWarnings("unchecked")
+public class LegalRepresentativeHearingRequirementsDirectionNotifierTest {
+
+    private static final String LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS_TEMPLATE = "template-id";
+
+    @Mock private LegalRepresentativePersonalisationFactory legalRepresentativePersonalisationFactory;
+    @Mock private DirectionFinder directionFinder;
+    @Mock private NotificationSender notificationSender;
+
+    @Mock private Callback<AsylumCase> callback;
+    @Mock private CaseDetails<AsylumCase> caseDetails;
+    @Mock private AsylumCase asylumCase;
+    @Mock private Direction legalRepresentativeHearingRequirementsDirection;
+    @Mock private Map<String, String> personalisation;
+
+    @Captor private ArgumentCaptor<List<IdValue<String>>> existingNotificationsSentCaptor;
+
+    final long caseId = 123L;
+
+    final String legalRepresentativeEmailAddress = "legal-representative@example.com";
+
+    final String expectedNotificationId = "ABC-DEF-GHI-JKL";
+    final String expectedNotificationReference = caseId + "_LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS_DIRECTION";
+
+    private LegalRepresentativeHearingRequirementsDirectionNotifier legalRepresentativeHearingRequirementsDirectionNotifier;
+
+    @Before
+    public void setUp() {
+        legalRepresentativeHearingRequirementsDirectionNotifier =
+            new LegalRepresentativeHearingRequirementsDirectionNotifier(
+                LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS_TEMPLATE,
+                legalRepresentativePersonalisationFactory,
+                directionFinder,
+                notificationSender
+            );
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(callback.getEvent()).thenReturn(Event.REQUEST_HEARING_REQUIREMENTS);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(caseDetails.getId()).thenReturn(caseId);
+        when(asylumCase.getLegalRepresentativeEmailAddress()).thenReturn(Optional.of(legalRepresentativeEmailAddress));
+        when(asylumCase.getNotificationsSent()).thenReturn(Optional.empty());
+
+        when(directionFinder.findFirst(asylumCase, DirectionTag.LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS)).thenReturn(Optional.of(legalRepresentativeHearingRequirementsDirection));
+        when(legalRepresentativePersonalisationFactory.create(asylumCase, legalRepresentativeHearingRequirementsDirection)).thenReturn(personalisation);
+
+        when(notificationSender.sendEmail(
+            LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS_TEMPLATE,
+            legalRepresentativeEmailAddress,
+            personalisation,
+            expectedNotificationReference
+        )).thenReturn(expectedNotificationId);
+    }
+
+    @Test
+    public void should_send_legal_rep_hearing_requirements_direction_notification() {
+
+        final List<IdValue<String>> existingNotifications =
+            new ArrayList<>(Arrays.asList(
+                new IdValue<>("some-notification-sent", "ZZZ-ZZZ-ZZZ-ZZZ")
+            ));
+
+        when(asylumCase.getNotificationsSent()).thenReturn(Optional.of(existingNotifications));
+
+        PreSubmitCallbackResponse<AsylumCase> callbackResponse =
+            legalRepresentativeHearingRequirementsDirectionNotifier.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
+
+        assertNotNull(callbackResponse);
+        assertEquals(asylumCase, callbackResponse.getData());
+
+        verify(notificationSender, times(1)).sendEmail(
+            LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS_TEMPLATE,
+            legalRepresentativeEmailAddress,
+            personalisation,
+            expectedNotificationReference
+        );
+
+        verify(asylumCase, times(1)).setNotificationsSent(existingNotificationsSentCaptor.capture());
+
+        List<IdValue<String>> actualExistingNotificationsSent =
+            existingNotificationsSentCaptor
+                .getAllValues()
+                .get(0);
+
+        assertEquals(2, actualExistingNotificationsSent.size());
+
+        assertEquals("some-notification-sent", actualExistingNotificationsSent.get(0).getId());
+        assertEquals("ZZZ-ZZZ-ZZZ-ZZZ", actualExistingNotificationsSent.get(0).getValue());
+
+        assertEquals(caseId + "_LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS_DIRECTION", actualExistingNotificationsSent.get(1).getId());
+        assertEquals(expectedNotificationId, actualExistingNotificationsSent.get(1).getValue());
+    }
+
+    @Test
+    public void should_send_legal_rep_hearing_requirements_direction_notification_when_no_notifications_exist() {
+
+        when(asylumCase.getNotificationsSent()).thenReturn(Optional.empty());
+
+        PreSubmitCallbackResponse<AsylumCase> callbackResponse =
+            legalRepresentativeHearingRequirementsDirectionNotifier.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
+
+        assertNotNull(callbackResponse);
+        assertEquals(asylumCase, callbackResponse.getData());
+
+        verify(notificationSender, times(1)).sendEmail(
+            LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS_TEMPLATE,
+            legalRepresentativeEmailAddress,
+            personalisation,
+            expectedNotificationReference
+        );
+
+        verify(asylumCase, times(1)).setNotificationsSent(existingNotificationsSentCaptor.capture());
+
+        List<IdValue<String>> actualExistingNotificationsSent =
+            existingNotificationsSentCaptor
+                .getAllValues()
+                .get(0);
+
+        assertEquals(1, actualExistingNotificationsSent.size());
+
+        assertEquals(caseId + "_LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS_DIRECTION", actualExistingNotificationsSent.get(0).getId());
+        assertEquals(expectedNotificationId, actualExistingNotificationsSent.get(0).getValue());
+    }
+
+    @Test
+    public void should_throw_when_legal_representative_hearing_requirements_direction_not_present() {
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(callback.getEvent()).thenReturn(Event.REQUEST_HEARING_REQUIREMENTS);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(directionFinder.findFirst(asylumCase, DirectionTag.LEGAL_REPRESENTATIVE_HEARING_REQUIREMENTS)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> legalRepresentativeHearingRequirementsDirectionNotifier.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback))
+            .hasMessage("legal representative hearing requirements direction is not present")
+            .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void should_throw_when_legal_representative_email_address_not_present() {
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(callback.getEvent()).thenReturn(Event.REQUEST_HEARING_REQUIREMENTS);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(asylumCase.getLegalRepresentativeEmailAddress()).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> legalRepresentativeHearingRequirementsDirectionNotifier.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback))
+            .hasMessage("legalRepresentativeEmailAddress is not present")
+            .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void handling_should_throw_if_cannot_actually_handle() {
+
+        assertThatThrownBy(() -> legalRepresentativeHearingRequirementsDirectionNotifier.handle(PreSubmitCallbackStage.ABOUT_TO_START, callback))
+            .hasMessage("Cannot handle callback")
+            .isExactlyInstanceOf(IllegalStateException.class);
+
+        when(callback.getEvent()).thenReturn(Event.SUBMIT_APPEAL);
+        assertThatThrownBy(() -> legalRepresentativeHearingRequirementsDirectionNotifier.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback))
+            .hasMessage("Cannot handle callback")
+            .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void it_can_handle_callback() {
+
+        for (Event event : Event.values()) {
+
+            when(callback.getEvent()).thenReturn(event);
+
+            for (PreSubmitCallbackStage callbackStage : PreSubmitCallbackStage.values()) {
+
+                boolean canHandle = legalRepresentativeHearingRequirementsDirectionNotifier.canHandle(callbackStage, callback);
+
+                if (event == Event.REQUEST_HEARING_REQUIREMENTS
+                    && callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT) {
+
+                    assertTrue(canHandle);
+                } else {
+                    assertFalse(canHandle);
+                }
+            }
+
+            reset(callback);
+        }
+    }
+
+    @Test
+    public void should_not_allow_null_arguments() {
+
+        assertThatThrownBy(() -> legalRepresentativeHearingRequirementsDirectionNotifier.canHandle(null, callback))
+            .hasMessage("callbackStage must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> legalRepresentativeHearingRequirementsDirectionNotifier.canHandle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, null))
+            .hasMessage("callback must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> legalRepresentativeHearingRequirementsDirectionNotifier.handle(null, callback))
+            .hasMessage("callbackStage must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> legalRepresentativeHearingRequirementsDirectionNotifier.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, null))
+            .hasMessage("callback must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+    }
+}
