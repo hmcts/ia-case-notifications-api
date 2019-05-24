caa2235d9e002802c44b29343f0b44fcdbc8a089
Ravi Aghera <Ravi.Aghera@HMCTS.net:wq>
2019-05-24 15:55:31 +0100

    Helm Charts - Adding chart config and dependencies
    bumping jackson version
    
    Modifying Dockerfile
    
    Adding configuration in default values.json
    
    updating Dockerfile
    
    bumping spring versions
    
    Adding liveness endpoint
    
    Adding liveness config
    
    Adding more environment config

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/GovNotifyConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/GovNotifyConfiguration.java
@@ -2,12 +2,14 @@ package uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.config;
 
 import static java.util.Objects.requireNonNull;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import uk.gov.service.notify.NotificationClient;
 
+@Slf4j
 @Configuration
 public class GovNotifyConfiguration {
 
@@ -16,6 +18,7 @@ public class GovNotifyConfiguration {
     public NotificationClient notificationClient(
         @Value("${govnotify.key}") String key
     ) {
+        log.info("\n\n>>> Setting Bean value to {} \n\n", key);
         requireNonNull(key);
         return new NotificationClient(key);
     }
