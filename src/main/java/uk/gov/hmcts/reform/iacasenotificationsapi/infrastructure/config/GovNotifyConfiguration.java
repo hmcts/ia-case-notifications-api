ed5158f40b1ca530b3eba3f45460d0bf15f17d32
Ravi Aghera <Ravi.Aghera@HMCTS.net:wq>
2019-06-05 16:38:40 +0100

    Tidying up

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/GovNotifyConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/GovNotifyConfiguration.java
@@ -2,14 +2,12 @@ package uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.config;
 
 import static java.util.Objects.requireNonNull;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import uk.gov.service.notify.NotificationClient;
 
-@Slf4j
 @Configuration
 public class GovNotifyConfiguration {
 
@@ -18,7 +16,6 @@ public class GovNotifyConfiguration {
     public NotificationClient notificationClient(
         @Value("${govnotify.key}") String key
     ) {
-        log.info("\n\n>>> Setting Bean value to {} \n\n", key);
         requireNonNull(key);
         return new NotificationClient(key);
     }
--- src/smokeTest/java/uk/gov/hmcts/reform/iacasenotificationsapi/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/iacasenotificationsapi/SmokeTest.java
@@ -3,14 +3,11 @@ package uk.gov.hmcts.reform.iacasenotificationsapi;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
-import java.util.Properties;
-import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.rest.SerenityRest;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 
-@Slf4j
 public class SmokeTest {
 
     private final String targetInstance =
@@ -21,18 +18,6 @@ public class SmokeTest {
 
     @Test
     public void should_prove_app_is_running_and_healthy() {
-
-        log.info("\n\n Smoke test request to url: {} ", targetInstance);
-
-        String s2s = StringUtils.defaultIfBlank(
-            System.getenv("S2S_URL"),
-            "http://localhost:8093"
-        );
-
-        log.info("\n\n S2S url: {} ", s2s);
-
-        Properties properties = System.getProperties();
-        properties.forEach((o, o2) -> log.info("name: {}, value: {}", o, o2));
 
         RestAssured.baseURI = targetInstance;
         RestAssured.useRelaxedHTTPSValidation();
