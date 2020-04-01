13cb6c55987964fa6aaa5d98a3a0da7f14108f02
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2020-04-01 13:16:51 +0100

    RIA-2775: Force case progression to case under review notification.
     - New notifcation handler for force case to case under review event.

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
@@ -52,6 +52,7 @@ public class EventTest {
         assertEquals("reviewTimeExtension", Event.REVIEW_TIME_EXTENSION.toString());
         assertEquals("sendDirectionWithQuestions", Event.SEND_DIRECTION_WITH_QUESTIONS.toString());
         assertEquals("submitClarifyingQuestionAnswers", Event.SUBMIT_CLARIFYING_QUESTION_ANSWERS.toString());
+        assertEquals("forceCaseToCaseUnderReview", Event.FORCE_CASE_TO_CASE_UNDER_REVIEW.toString());
         assertEquals("unknown", Event.UNKNOWN.toString());
     }
 
