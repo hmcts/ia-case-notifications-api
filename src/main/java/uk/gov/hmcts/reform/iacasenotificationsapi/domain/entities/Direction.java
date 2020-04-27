48cd15358e3cc4aef59c0c0eaeb6cabae4883f71
Alejandro Baeza <alejandro.baeza@hmcts.net>
2020-04-27 11:07:55 +0100

    Fixing Directions allowing null explanation

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/Direction.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/Direction.java
@@ -27,6 +27,7 @@ public class Direction {
         DirectionTag tag,
         List<IdValue<PreviousDates>> previousDates
     ) {
+        requireNonNull(explanation);
         requireNonNull(parties);
         requireNonNull(dateDue);
         requireNonNull(dateSent);
