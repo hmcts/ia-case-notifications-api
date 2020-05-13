052ed02187a98b5c378d1d9d5a89e5746bca00f5
Brad Sorour <brad.sorour@gmail.com>
2020-05-13 13:00:37 +0100

    RIA-1432 merge

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
@@ -49,11 +49,12 @@ public class EventTest {
         assertEquals("applyForFTPAAppellant", Event.APPLY_FOR_FTPA_APPELLANT.toString());
         assertEquals("applyForFTPARespondent", Event.APPLY_FOR_FTPA_RESPONDENT.toString());
         assertEquals("submitTimeExtension", Event.SUBMIT_TIME_EXTENSION.toString());
+        assertEquals("leadershipJudgeFtpaDecision", Event.LEADERSHIP_JUDGE_FTPA_DECISION.toString());
         assertEquals("unknown", Event.UNKNOWN.toString());
     }
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(43, Event.values().length);
+        assertEquals(44, Event.values().length);
     }
 }
