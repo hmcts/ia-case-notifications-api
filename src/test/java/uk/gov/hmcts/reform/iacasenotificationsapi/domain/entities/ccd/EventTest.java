a40d16a30c221e664340cbfbaa52af049ccc8666
david.crespo <david.crespo@hmcts.net>
2020-07-15 13:15:47 +0200

    fix conflicts

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
@@ -65,6 +65,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(53, Event.values().length);
+        assertEquals(54, Event.values().length);
     }
 }
