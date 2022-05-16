29b17497145c6c620c3dea12a5237243a63d9d57
indu2019 <indu.chaturvedi@hmcts.net>
2022-05-16 11:26:45 +0100

    RIA-5679-Indu added a test case about which sonarqube was complaining about

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/security/idam/IdamAuthoritiesConverterTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/security/idam/IdamAuthoritiesConverterTest.java
@@ -32,7 +32,6 @@ public class IdamAuthoritiesConverterTest {
 
     @Mock
     private UserInfo userInfo;
-
     private String tokenValue =
         "eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FIiwia2lkIjoiYi9PNk92VnYxK3krV2dySDVVaTlXVGlvTHQwPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJjY2QtaW1wb3J0QGZha2UuaG1jdHMubmV0IiwiYXV0aF9sZXZlbCI6MCwiYXVkaXRUcmFja2luZ0lkIjoiZDg3ODI3ODQtMWU0NC00NjkyLTg0NzgtNTI5MzE0NTVhNGI5IiwiaXNzIjoiaHR0cDovL2ZyLWFtOjgwODAvb3BlbmFtL29hdXRoMi9obWN0cyIsInRva2VuTmFtZSI6ImFjY2Vzc190b2tlbiIsInRva2VuX3R5cGUiOiJCZWFyZXIiLCJhdXRoR3JhbnRJZCI6IjNjMWMzNjFkLTRlYzUtNGY0NS1iYzI0LTUxOGMzMDk0MzUxYiIsImF1ZCI6ImNjZF9nYXRld2F5IiwibmJmIjoxNTg0NTI2MzcyLCJncmFudF90eXBlIjoicGFzc3dvcmQiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwicm9sZXMiXSwiYXV0aF90aW1lIjoxNTg0NTI2MzcyLCJyZWFsbSI6Ii9obWN0cyIsImV4cCI6MTU4NDU1NTE3MiwiaWF0IjoxNTg0NTI2MzcyLCJleHBpcmVzX2luIjoyODgwMCwianRpIjoiNDhjNDMzYTQtZmRiNS00YTIwLWFmNGUtMmYyNjIyYjYzZmU5In0.WP8ATcHMmdtG2W443aqNz3ES6-Bqng0IKjTQfbndN1HrBLJWJtpC3qfzy2wD_CdiPU4uspdN5S91nhiT8Ub6DjstnDz3VPmR3Cbdk5QJBdAsQ0ah9w-duS8SA_dlzDIMt18bSDMUUdck6YxsoNyQFisI6cKNnfgB9ZTLhenVENtdmyrKVr96Ezp-jhhzmMVMxb1rW7KghSAH0ZCsWqlhrM--jPGRCweDiFe-ldi4EuhIxGbkPjyWwsdcgmYfIuFrSxqV0vrSI37DNZx_Sh5DVJpUgSrYKRzuMqe4rFN6WVyHIY_Qu52ER2yrNYtGbAQ5AyMabPTPj9VVxqpa5nYUAg";
 
@@ -44,6 +43,9 @@ public class IdamAuthoritiesConverterTest {
         when(jwt.hasClaim(TOKEN_NAME)).thenReturn(true);
         when(jwt.getClaim(TOKEN_NAME)).thenReturn(ACCESS_TOKEN);
         when(jwt.getTokenValue()).thenReturn(tokenValue);
+
+        assertTrue(jwt.hasClaim(TOKEN_NAME));
+        assertEquals(jwt.getClaim(TOKEN_NAME).toString(), "access_token");
 
         when(userInfo.getRoles()).thenReturn(Lists.newArrayList("caseworker-ia", "caseworker-ia-caseofficer"));
         when(idamApi.userInfo("Bearer " + tokenValue)).thenReturn(userInfo);
@@ -91,6 +93,8 @@ public class IdamAuthoritiesConverterTest {
         when(jwt.hasClaim(TOKEN_NAME)).thenReturn(true);
         when(jwt.getClaim(TOKEN_NAME)).thenReturn(ACCESS_TOKEN);
         when(jwt.getTokenValue()).thenReturn(tokenValue);
+        assertTrue(jwt.hasClaim(TOKEN_NAME));
+        assertEquals(jwt.getClaim(TOKEN_NAME).toString(), "access_token");
 
         assertEquals(Collections.emptyList(), idamAuthoritiesConverter.convert(jwt));
     }
@@ -103,6 +107,9 @@ public class IdamAuthoritiesConverterTest {
         when(jwt.hasClaim(TOKEN_NAME)).thenReturn(true);
         when(jwt.getClaim(TOKEN_NAME)).thenReturn(ACCESS_TOKEN);
         when(jwt.getTokenValue()).thenReturn(tokenValue);
+
+        assertTrue(jwt.hasClaim(TOKEN_NAME));
+        assertEquals(jwt.getClaim(TOKEN_NAME).toString(), "access_token");
 
         idamAuthoritiesConverter = new IdamAuthoritiesConverter(idamApi);
 
