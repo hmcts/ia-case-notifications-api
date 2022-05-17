236ddc22f87a2fda9f639c1cce5864a0a3ec978c
Prem Shrivastava <prem.shrivastava@version1.com>
2022-05-17 11:35:55 +0100

    RIA-5325 fixed reported vulnerabilities

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/Application.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.iacasenotificationsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
