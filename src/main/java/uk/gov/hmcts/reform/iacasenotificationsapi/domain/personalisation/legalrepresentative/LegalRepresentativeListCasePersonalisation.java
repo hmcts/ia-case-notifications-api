15dc378d20ddfe2cd80e226dac21d9f350888567
Marcellofabbri <marcello.fabbri@hmcts.net>
2022-12-07 15:15:22 +0000

    RIA-6533 ADA listCase direction notification

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/legalrepresentative/LegalRepresentativeListCasePersonalisation.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/legalrepresentative/LegalRepresentativeListCasePersonalisation.java
@@ -5,11 +5,17 @@ import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumC
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.Direction;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.DirectionTag;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.HearingCentre;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.field.YesOrNo;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.DirectionFinder;
 import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.*;
 
 @Service
@@ -17,30 +23,40 @@ public class LegalRepresentativeListCasePersonalisation implements LegalRepresen
 
     private final String legalRepresentativeCaseListedTemplateId;
     private final String legalRepresentativeOutOfCountryCaseListedTemplateId;
+    private final String legalRepresentativeAdaCaseListedTemplateId;
     private final String iaExUiFrontendUrl;
     private final DateTimeExtractor dateTimeExtractor;
     private final CustomerServicesProvider customerServicesProvider;
     private final HearingDetailsFinder hearingDetailsFinder;
+    private final DirectionFinder directionFinder;
 
 
     public LegalRepresentativeListCasePersonalisation(
         @Value("${govnotify.template.caseListed.legalRep.email}") String legalRepresentativeCaseListedTemplateId,
         @Value("${govnotify.template.caseListed.remoteHearing.legalRep.email}") String legalRepresentativeOutOfCountryCaseListedTemplateId,
+        @Value("${govnotify.template.adaCaseListed.legalRep.email}") String legalRepresentativeAdaCaseListedTemplateId,
         @Value("${iaExUiFrontendUrl}") String iaExUiFrontendUrl,
         DateTimeExtractor dateTimeExtractor,
         CustomerServicesProvider customerServicesProvider,
-        HearingDetailsFinder hearingDetailsFinder
+        HearingDetailsFinder hearingDetailsFinder,
+        DirectionFinder directionFinder
     ) {
         this.legalRepresentativeCaseListedTemplateId = legalRepresentativeCaseListedTemplateId;
         this.legalRepresentativeOutOfCountryCaseListedTemplateId = legalRepresentativeOutOfCountryCaseListedTemplateId;
+        this.legalRepresentativeAdaCaseListedTemplateId = legalRepresentativeAdaCaseListedTemplateId;
         this.iaExUiFrontendUrl = iaExUiFrontendUrl;
         this.dateTimeExtractor = dateTimeExtractor;
         this.customerServicesProvider = customerServicesProvider;
         this.hearingDetailsFinder = hearingDetailsFinder;
+        this.directionFinder = directionFinder;
     }
 
     @Override
     public String getTemplateId(AsylumCase asylumCase) {
+        if (isAcceleratedDetainedAppeal(asylumCase)) {
+            return legalRepresentativeAdaCaseListedTemplateId;
+        }
+
         if (asylumCase.read(LIST_CASE_HEARING_CENTRE, HearingCentre.class)
             .map(centre -> centre == HearingCentre.REMOTE_HEARING)
             .orElse(false)) {
@@ -59,22 +75,53 @@ public class LegalRepresentativeListCasePersonalisation implements LegalRepresen
     public Map<String, String> getPersonalisation(AsylumCase asylumCase) {
         requireNonNull(asylumCase, "asylumCase must not be null");
 
-        final Builder<String, String> listCaseFields = ImmutableMap
-            .<String, String>builder()
-            .putAll(customerServicesProvider.getCustomerServicesPersonalisation())
-            .put("appealReferenceNumber", asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class).orElse(""))
-            .put("ariaListingReference", asylumCase.read(ARIA_LISTING_REFERENCE, String.class).orElse(""))
-            .put("legalRepReferenceNumber", asylumCase.read(LEGAL_REP_REFERENCE_NUMBER, String.class).orElse(""))
-            .put("appellantGivenNames", asylumCase.read(APPELLANT_GIVEN_NAMES, String.class).orElse(""))
-            .put("appellantFamilyName", asylumCase.read(APPELLANT_FAMILY_NAME, String.class).orElse(""))
-            .put("linkToOnlineService", iaExUiFrontendUrl)
-            .put("hearingDate", dateTimeExtractor.extractHearingDate(hearingDetailsFinder.getHearingDateTime(asylumCase)))
-            .put("hearingTime", dateTimeExtractor.extractHearingTime(hearingDetailsFinder.getHearingDateTime(asylumCase)))
-            .put("hearingCentreAddress", hearingDetailsFinder.getHearingCentreLocation(asylumCase));
+        Builder<String, String> listCaseFields;
+
+        if (isAcceleratedDetainedAppeal(asylumCase)) {
+            final Direction direction =
+                directionFinder
+                    .findFirst(asylumCase, DirectionTag.ADA_LIST_CASE)
+                    .orElseThrow(() -> new IllegalStateException("LR List ADA Case direction is not present"));
+
+            listCaseFields = ImmutableMap
+                .<String, String>builder()
+                .putAll(customerServicesProvider.getCustomerServicesPersonalisation())
+                .put("appealReferenceNumber", asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class).orElse(""))
+                .put("ariaListingReference", asylumCase.read(ARIA_LISTING_REFERENCE, String.class).orElse(""))
+                .put("legalRepReferenceNumber", asylumCase.read(LEGAL_REP_REFERENCE_NUMBER, String.class).orElse(""))
+                .put("appellantGivenNames", asylumCase.read(APPELLANT_GIVEN_NAMES, String.class).orElse(""))
+                .put("appellantFamilyName", asylumCase.read(APPELLANT_FAMILY_NAME, String.class).orElse(""))
+                .put("explanation", direction.getExplanation())
+                .put("linkToOnlineService", iaExUiFrontendUrl)
+                .put("hearingCentreAddress", hearingDetailsFinder.getHearingCentreLocation(asylumCase));
+        } else {
+            listCaseFields = ImmutableMap
+                .<String, String>builder()
+                .putAll(customerServicesProvider.getCustomerServicesPersonalisation())
+                .put("appealReferenceNumber", asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class).orElse(""))
+                .put("ariaListingReference", asylumCase.read(ARIA_LISTING_REFERENCE, String.class).orElse(""))
+                .put("legalRepReferenceNumber", asylumCase.read(LEGAL_REP_REFERENCE_NUMBER, String.class).orElse(""))
+                .put("appellantGivenNames", asylumCase.read(APPELLANT_GIVEN_NAMES, String.class).orElse(""))
+                .put("appellantFamilyName", asylumCase.read(APPELLANT_FAMILY_NAME, String.class).orElse(""))
+                .put("linkToOnlineService", iaExUiFrontendUrl)
+                .put("hearingDate", dateTimeExtractor.extractHearingDate(hearingDetailsFinder.getHearingDateTime(asylumCase)))
+                .put("hearingTime", dateTimeExtractor.extractHearingTime(hearingDetailsFinder.getHearingDateTime(asylumCase)))
+                .put("hearingCentreAddress", hearingDetailsFinder.getHearingCentreLocation(asylumCase));
+        }
 
         PersonalisationProvider.buildHearingRequirementsFields(asylumCase, listCaseFields);
 
         return listCaseFields.build();
 
+    }
+
+    private boolean isAcceleratedDetainedAppeal(AsylumCase asylumCase) {
+        return asylumCase.read(IS_ACCELERATED_DETAINED_APPEAL, YesOrNo.class)
+            .orElse(YesOrNo.NO)
+            .equals(YesOrNo.YES);
+    }
+
+    private String calculateDueDate(Long days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
     }
 }
