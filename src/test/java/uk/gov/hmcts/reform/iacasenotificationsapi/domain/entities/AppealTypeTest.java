510a26e41bf352abcddf2ad37da0347be65f4443
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2022-12-12 15:12:23 +0000

    Updated appeal type test to fix  build failure

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/AppealTypeTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/AppealTypeTest.java
@@ -25,6 +25,6 @@ public class AppealTypeTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(6, AppealType.values().length);
+        assertEquals(7, AppealType.values().length);
     }
 }
