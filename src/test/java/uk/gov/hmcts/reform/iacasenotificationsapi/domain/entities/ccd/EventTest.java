e095e5ac5b3776fef31c1cde5bf9e8657a3a5635
Simon Buckley <106660286+sibuckley-v1@users.noreply.github.com>
2022-12-12 15:21:52 +0000

    Fix test

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
@@ -108,6 +108,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(94, Event.values().length);
+        assertEquals(96, Event.values().length);
     }
 }
