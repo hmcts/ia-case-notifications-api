0104d3401158ea3c3af903c3deadf117aae23532
Jephtah Addison <Jephtah.Addison@HMCTS.NET>
2023-01-17 16:33:53 +0000

    fix for test after rebasing

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/email/AppellantForceCaseProgressionToCaseUnderReviewPersonalisationEmailTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/email/AppellantForceCaseProgressionToCaseUnderReviewPersonalisationEmailTest.java
@@ -41,7 +41,7 @@ class AppellantForceCaseProgressionToCaseUnderReviewPersonalisationEmailTest {
     @BeforeEach
     void setUp() {
         when(asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class)).thenReturn(Optional.of(appealReferenceNumber));
-        when(asylumCase.read(APPELLANT_EMAIL_ADDRESS, String.class)).thenReturn(Optional.of(appellantEmailAddress));
+        when(asylumCase.read(EMAIL, String.class)).thenReturn(Optional.of(appellantEmailAddress));
         when(asylumCase.read(APPELLANT_GIVEN_NAMES, String.class)).thenReturn(Optional.of(appellantGivenNames));
         when(asylumCase.read(APPELLANT_FAMILY_NAME, String.class)).thenReturn(Optional.of(appellantFamilyName));
 
@@ -63,7 +63,7 @@ class AppellantForceCaseProgressionToCaseUnderReviewPersonalisationEmailTest {
     @Test
     public void should_return_given_email_address_from_asylum_case() {
         Map<String, String> personalisation = forceCaseProgressionToCaseUnderReviewPersonalisation.getPersonalisation(asylumCase);
-        assertEquals(appellantEmailAddress, personalisation.get(APPELLANT_EMAIL_ADDRESS.value()));
+        assertEquals(appellantEmailAddress, personalisation.get(EMAIL.value()));
     }
 
     @Test
@@ -76,7 +76,7 @@ class AppellantForceCaseProgressionToCaseUnderReviewPersonalisationEmailTest {
 
     @Test
     void should_throw_an_illegal_state_exception_when_email_is_not_in_recipient_list() {
-        when(asylumCase.read(APPELLANT_EMAIL_ADDRESS, String.class)).thenReturn(Optional.empty());
+        when(asylumCase.read(EMAIL, String.class)).thenReturn(Optional.empty());
         assertThatThrownBy(() -> forceCaseProgressionToCaseUnderReviewPersonalisation.getRecipientsList(asylumCase))
                 .isExactlyInstanceOf(IllegalStateException.class)
                 .hasMessage("appellantEmailAddress is not present");
@@ -84,7 +84,7 @@ class AppellantForceCaseProgressionToCaseUnderReviewPersonalisationEmailTest {
 
     @Test
     public void should_throw_exception_when_appellant_email_is_not_present() {
-        when(asylumCase.read(APPELLANT_EMAIL_ADDRESS, String.class)).thenReturn(Optional.empty());
+        when(asylumCase.read(EMAIL, String.class)).thenReturn(Optional.empty());
         assertThatThrownBy(() -> forceCaseProgressionToCaseUnderReviewPersonalisation.getPersonalisation(asylumCase))
             .isExactlyInstanceOf(IllegalStateException.class)
             .hasMessage("appellantEmailAddress is not present");
