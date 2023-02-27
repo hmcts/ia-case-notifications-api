626fa53762ee4975e8e170efccd2ee3588697c3e
Augustine Onubeze <ikechukwu.onubeze@gmail.com>
2023-02-27 10:15:43 +0000

    RIA-6733: Don't send notification to Legal rep on 'markAppealPaid' event for internal cases (#769)

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -2234,7 +2234,8 @@ public class NotificationHandlerConfiguration {
                        && callback.getEvent() == Event.MARK_APPEAL_PAID
                        && (isCorrectAppealTypePA || isCorrectAppealTypeAndStateHUorEA)
                        && !paymentStatus.equals(Optional.empty())
-                       && paymentStatus.get().equals(PaymentStatus.PAID);
+                       && paymentStatus.get().equals(PaymentStatus.PAID)
+                       && !AsylumCaseUtils.isInternalCase(asylumCase);
             }, notificationGenerators
         );
     }
