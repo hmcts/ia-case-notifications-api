88a92e6fdbd76e5f64714cc0afad4a30235b9cce
Jamie <jamiemurphy@version1.com>
2023-03-08 11:46:13 +0000

    RIA-6779 Request respondent review

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/detentionengagementteam/DetentionEngagementTeamRespondentReviewPersonalisation.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/detentionengagementteam/DetentionEngagementTeamRespondentReviewPersonalisation.java
@@ -5,29 +5,35 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.*;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.EmailNotificationPersonalisation;
-import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.RecipientsFinder;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.DetEmailService;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.DirectionFinder;
 
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.LEGAL_REPRESENTATIVE_EMAIL_ADDRESS;
+
 
 @Service
 public class DetentionEngagementTeamRespondentReviewPersonalisation implements EmailNotificationPersonalisation {
 
     private final String detentionEngagementTeamRespondentReviewTemplateId;
-    private final RecipientsFinder recipientsFinder;
+    private final DirectionFinder directionFinder;
+    private final DetEmailService detEmailService;
 
     public DetentionEngagementTeamRespondentReviewPersonalisation(
         @NotNull(message = "DetentionEngagementTeamRespondentReviewTemplateId cannot be null")
         @Value("${govnotify.template.reviewDirection.detentionTeam.email}") String detentionEngagementTeamRespondentReviewTemplateId,
-        RecipientsFinder recipientsFinder) {
-
+        DirectionFinder directionFinder,
+        DetEmailService detEmailService
+    ) {
         this.detentionEngagementTeamRespondentReviewTemplateId = detentionEngagementTeamRespondentReviewTemplateId;
-        this.recipientsFinder = recipientsFinder;
+        this.directionFinder = directionFinder;
+        this.detEmailService = detEmailService;
     }
 
     @Override
@@ -42,12 +48,22 @@ public class DetentionEngagementTeamRespondentReviewPersonalisation implements E
 
     @Override
     public Set<String> getRecipientsList(final AsylumCase asylumCase) {
-        return Collections.singleton("somedummyemail@gmail.com");
+        return Collections.singleton(detEmailService.getAdaDetEmailAddress());
     }
 
     @Override
     public Map<String, String> getPersonalisation(AsylumCase asylumCase) {
         requireNonNull(asylumCase, "asylumCase must not be null");
+
+        final Direction direction =
+                directionFinder
+                        .findFirst(asylumCase, DirectionTag.RESPONDENT_REVIEW)
+                        .orElseThrow(() -> new IllegalStateException("direction '" + DirectionTag.RESPONDENT_REVIEW + "' is not present"));
+
+        final String directionDueDate =
+                LocalDate
+                        .parse(direction.getDateDue())
+                        .format(DateTimeFormatter.ofPattern("d MMM yyyy"));
 
         return ImmutableMap
             .<String, String>builder()
@@ -56,7 +72,7 @@ public class DetentionEngagementTeamRespondentReviewPersonalisation implements E
             .put("homeOfficeReferenceNumber", asylumCase.read(AsylumCaseDefinition.HOME_OFFICE_REFERENCE_NUMBER, String.class).orElse(""))
             .put("appellantGivenNames", asylumCase.read(AsylumCaseDefinition.APPELLANT_GIVEN_NAMES, String.class).orElse(""))
             .put("appellantFamilyName", asylumCase.read(AsylumCaseDefinition.APPELLANT_FAMILY_NAME, String.class).orElse(""))
-            .put("directionDueDate", asylumCase.read(AsylumCaseDefinition.SEND_DIRECTION_DATE_DUE, String.class).orElse(""))
+            .put("directionDueDate", directionDueDate)
             .build();
     }
 }
