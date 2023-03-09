29ab12918ea113c64797f7d015240952ffb97551
NehaAggarwal <neha.aggarwal@version1.com>
2023-03-09 16:36:37 +0000

    RIA-6864: Created EmailNotificationGenerator to accept personalisatio… (#772)
    * RIA-6864: Created EmailNotificationGenerator to accept personalisation as a Map<String, Object>
    
    - Upgraded gov.notify to 3.19.
    - Added interfaces for LRPersonalisation to use the new Personalisation
      map.
    - Added DocumentDownloadClient to download a document from dm-store.
    
    * RIA-6864: Remove personalisation created for testing the new flow
    
    * RIA-6864: Fix checkstyle errors
    
    * Added the package to fiegn client list
    
    * RIA-6864 - Fixed PACT tests
    
    * RIA-6864 - Fixed PACT tests
    
    * RIA-6864 - Fixed PACT tests
    
    * RIA-6864 - Fixed PACT tests
    
    * RIA-6864 - Fixed PACT tests
    
    * RIA-6864 - Fixed PACT tests
    
    * RIA-6864 - Fixed PACT tests
    
    * RIA-6864 - Fixed PACT tests
    
    * RIA-6864 - Fix build failure on preview
    
    * RIA-6864 - Fix build failure on preview
    
    * RIA-6864 - Fix build failure on preview
    
    * RIA-6864 - Fix build failure on preview
    
    * RIA-6864 - Fix build failure on preview
    
    * RIA-6864 - Revert out pact test changes
    
    * RIA-6864 - Revert out pact test changes
    
    * RIA-6864 - Revert out pact test changes
    
    * RIA-6864 - Removed unused config
    
    * RIA-6864 - Removed unused config
    
    ---------
    
    Co-authored-by: colmlynch60 <colm.lynch@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/service/EmailWithLinkNotificationGenerator.java
@@ -0,0 +1,77 @@
+package uk.gov.hmcts.reform.iacasenotificationsapi.domain.service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.EmailWithLinkNotificationPersonalisation;
+import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.clients.GovNotifyNotificationSender;
+
+public class EmailWithLinkNotificationGenerator implements NotificationGenerator {
+
+    protected final List<EmailWithLinkNotificationPersonalisation> personalisationList;
+    protected final NotificationIdAppender notificationIdAppender;
+    protected final GovNotifyNotificationSender notificationSender;
+
+    public EmailWithLinkNotificationGenerator(
+        List<EmailWithLinkNotificationPersonalisation> repPersonalisationList,
+        GovNotifyNotificationSender notificationSender,
+        NotificationIdAppender notificationIdAppender) {
+
+        this.personalisationList = repPersonalisationList;
+        this.notificationSender = notificationSender;
+        this.notificationIdAppender = notificationIdAppender;
+    }
+
+    @Override
+    public void generate(Callback<AsylumCase> callback) {
+
+        final AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+
+        personalisationList.forEach(personalisation -> {
+            String referenceId = personalisation.getReferenceId(callback.getCaseDetails().getId());
+            List<String> notificationIds = createEmail(personalisation, asylumCase, referenceId, callback);
+            notificationIdAppender.appendAll(asylumCase, referenceId, notificationIds);
+        });
+    }
+
+    protected List<String> createEmail(
+        final EmailWithLinkNotificationPersonalisation personalisation,
+        final AsylumCase asylumCase,
+        final String referenceId,
+        final Callback<AsylumCase> callback) {
+
+        EmailWithLinkNotificationPersonalisation emailNotificationPersonalisation = personalisation;
+        Set<String> subscriberEmails = emailNotificationPersonalisation.getRecipientsList(asylumCase);
+
+        return subscriberEmails.stream()
+            .map(email ->
+                sendEmail(
+                    email,
+                    emailNotificationPersonalisation,
+                    referenceId,
+                    callback)).collect(Collectors.toList());
+    }
+
+    protected String sendEmail(
+        final String email,
+        final EmailWithLinkNotificationPersonalisation personalisation,
+        final String referenceId,
+        final Callback<AsylumCase> callback) {
+
+        String emailTemplateId = personalisation.getTemplateId() == null
+            ?
+            personalisation.getTemplateId(callback.getCaseDetails().getCaseData()) : personalisation.getTemplateId();
+
+
+
+        return notificationSender.sendEmailWithLink(
+                        emailTemplateId,
+                        email,
+                        personalisation.getPersonalisationForLink(callback),
+                        referenceId
+                );
+    }
+
+}
