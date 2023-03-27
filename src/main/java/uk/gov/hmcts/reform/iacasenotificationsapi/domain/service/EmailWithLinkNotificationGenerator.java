e89cd1991b19ed4e3873198f6d1bab98804f7cab
Marcello Fabbri <56549133+Marcellofabbri@users.noreply.github.com>
2023-03-27 09:05:08 +0100

    RIA-6858 Make CustomerServicesProvider pick between different emails (#796)
    * RIA-6858 Make CustomerServicesProvider pick between different emails
    
    * RIA-6858 Change test for ApplicationContextProvider
    
    * RIA-6858 Mark setCorrectEmail synchronized
    
    * RIA-6858 Mark CustomerServicesProvider's scope as "prototype"
    
    * RIA-6858 Unmark bean as "prototype" and bean manipulation upwards
    
    * RIA-6858 Add bean manipulation to other implementations of .generate()
    
    * RIA-6858 Reorganize MockedStatic in tests
    
    * RIA-6858 Edit ID of case in functional test

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/service/EmailWithLinkNotificationGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/service/EmailWithLinkNotificationGenerator.java
@@ -3,9 +3,11 @@ package uk.gov.hmcts.reform.iacasenotificationsapi.domain.service;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.ApplicationContextProvider;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.EmailWithLinkNotificationPersonalisation;
+import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.CustomerServicesProvider;
 import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.clients.GovNotifyNotificationSender;
 
 public class EmailWithLinkNotificationGenerator implements NotificationGenerator {
@@ -28,6 +30,9 @@ public class EmailWithLinkNotificationGenerator implements NotificationGenerator
     public void generate(Callback<AsylumCase> callback) {
 
         final AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+
+        ApplicationContextProvider.getApplicationContext().getBean(CustomerServicesProvider.class)
+            .setCorrectEmail(asylumCase);
 
         personalisationList.forEach(personalisation -> {
             String referenceId = personalisation.getReferenceId(callback.getCaseDetails().getId());
