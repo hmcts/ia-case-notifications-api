8f165e0b69549753d4674c9970750ed22a7563c2
Marcellofabbri <marcello.fabbri@hmcts.net>
2023-04-20 13:01:33 +0100

    RIA-6135 Add notifications for refused and not admitted ftpa decisions

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/sms/AppellantRespondentFtpaApplicationDecisionPersonalisationSms.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/sms/AppellantRespondentFtpaApplicationDecisionPersonalisationSms.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.appellant.sms;
 
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.APPEAL_REFERENCE_NUMBER;
-import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.utils.PersonalisationUtil.ftpaRespondentDecisionVerbalization;
 
 import com.google.common.collect.ImmutableMap;
 import java.util.Map;
@@ -9,30 +8,49 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.FtpaDecisionOutcomeType;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.NotificationType;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.SmsNotificationPersonalisation;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.utils.FtpaNotificationPersonalisationUtil;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.RecipientsFinder;
 
 @Service
-public class AppellantRespondentFtpaApplicationDecisionPersonalisationSms implements SmsNotificationPersonalisation {
+public class AppellantRespondentFtpaApplicationDecisionPersonalisationSms implements SmsNotificationPersonalisation, FtpaNotificationPersonalisationUtil {
 
-    private final String appellantFtpaRespondentDecisionSmsTemplateId;
+    private final String ftpaRespondentDecisionGrantedPartiallyGrantedToAppellantSmsTemplateId;
+    private final String ftpaRespondentDecisionGrantedNotAdmittedToAppellantSmsTemplateId;
+    private final String ftpaRespondentDecisionGrantedRefusedToAppellantSmsTemplateId;
     private final String iaAipFrontendUrl;
     private final RecipientsFinder recipientsFinder;
 
     public AppellantRespondentFtpaApplicationDecisionPersonalisationSms(
-        @Value("${govnotify.template.applicationGranted.otherParty.citizen.sms}") String appellantFtpaRespondentDecisionSmsTemplateId,
+        @Value("${govnotify.template.applicationGranted.otherParty.citizen.sms}") String ftpaRespondentDecisionGrantedPartiallyGrantedToAppellantSmsTemplateId,
+        @Value("${govnotify.template.applicationNotAdmitted.otherParty.citizen.sms}") String ftpaRespondentDecisionGrantedNotAdmittedToAppellantSmsTemplateId,
+        @Value("${govnotify.template.applicationRefused.otherParty.citizen.sms}") String ftpaRespondentDecisionGrantedRefusedToAppellantSmsTemplateId,
         @Value("${iaAipFrontendUrl}") String iaAipFrontendUrl,
         RecipientsFinder recipientsFinder
     ) {
-        this.appellantFtpaRespondentDecisionSmsTemplateId = appellantFtpaRespondentDecisionSmsTemplateId;
+        this.ftpaRespondentDecisionGrantedPartiallyGrantedToAppellantSmsTemplateId = ftpaRespondentDecisionGrantedPartiallyGrantedToAppellantSmsTemplateId;
+        this.ftpaRespondentDecisionGrantedNotAdmittedToAppellantSmsTemplateId = ftpaRespondentDecisionGrantedNotAdmittedToAppellantSmsTemplateId;
+        this.ftpaRespondentDecisionGrantedRefusedToAppellantSmsTemplateId = ftpaRespondentDecisionGrantedRefusedToAppellantSmsTemplateId;
         this.iaAipFrontendUrl = iaAipFrontendUrl;
         this.recipientsFinder = recipientsFinder;
     }
 
     @Override
-    public String getTemplateId() {
-        return appellantFtpaRespondentDecisionSmsTemplateId;
+    public String getTemplateId(AsylumCase asylumCase) {
+        FtpaDecisionOutcomeType decisionOutcomeType = ftpaRespondentLjRjDecision(asylumCase)
+            .orElseThrow(() -> new IllegalStateException("ftpaRespondentDecisionOutcomeType is not present"));
+
+        switch (decisionOutcomeType) {
+            case FTPA_GRANTED:
+            case FTPA_PARTIALLY_GRANTED:
+                return ftpaRespondentDecisionGrantedPartiallyGrantedToAppellantSmsTemplateId;
+            case FTPA_REFUSED:
+                return ftpaRespondentDecisionGrantedRefusedToAppellantSmsTemplateId;
+            default:
+                return ftpaRespondentDecisionGrantedNotAdmittedToAppellantSmsTemplateId;
+        }
     }
 
     @Override
@@ -51,7 +69,7 @@ public class AppellantRespondentFtpaApplicationDecisionPersonalisationSms implem
         return ImmutableMap
             .<String, String>builder()
             .put("appealReferenceNumber", asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class).orElse(""))
-            .put("applicationDecision", ftpaRespondentDecisionVerbalization(asylumCase))
+            .put("applicationDecision", ftpaDecisionVerbalization(asylumCase))
             .put("linkToService", iaAipFrontendUrl)
             .build();
     }
