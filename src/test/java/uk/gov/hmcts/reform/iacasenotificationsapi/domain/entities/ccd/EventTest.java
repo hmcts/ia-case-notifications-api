6c89c2b1cc6fd433e82d3435145d47085118388f
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2023-05-09 17:39:44 +0100

    Update EventTest.java

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
@@ -111,6 +111,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(98, Event.values().length);
+        assertEquals(99, Event.values().length);
     }
 }
