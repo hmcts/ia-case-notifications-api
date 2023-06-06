add5751afcd481fd7bced8a54eb089d609939717
Ryan-Nesbitt <95298094+Ryan-Nesbitt@users.noreply.github.com>
2023-06-06 20:11:30 +0100

    RIA-7044 Adding HO Reference number to personalisation for UT notific… (#879)
    * RIA-7044 Adding HO Reference number to personalisation for UT notification
    
    * Bumping chart version/ fixing aliases
    
    * RIA-7044 Unit test fix
    
    ---------
    
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/uppertribunal/UpperTribunalMarkAsReadyForUtTransferPersonalisation.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/uppertribunal/UpperTribunalMarkAsReadyForUtTransferPersonalisation.java
@@ -64,8 +64,8 @@ public class UpperTribunalMarkAsReadyForUtTransferPersonalisation implements Ema
         return ImmutableMap
             .<String, String>builder()
             .putAll(customerServicesProvider.getCustomerServicesPersonalisation())
-            .put("homeOfficeReferenceNumber", asylumCase.read(AsylumCaseDefinition.HOME_OFFICE_REFERENCE_NUMBER, String.class).orElse(""))
             .put("appealReferenceNumber", asylumCase.read(AsylumCaseDefinition.APPEAL_REFERENCE_NUMBER, String.class).orElse(""))
+            .put("homeOfficeReferenceNumber", asylumCase.read(AsylumCaseDefinition.HOME_OFFICE_REFERENCE_NUMBER, String.class).orElse(""))
             .put("ariaListingReference", asylumCase.read(AsylumCaseDefinition.ARIA_LISTING_REFERENCE, String.class).orElse(""))
             .put("appellantGivenNames", asylumCase.read(AsylumCaseDefinition.APPELLANT_GIVEN_NAMES, String.class).orElse(""))
             .put("appellantFamilyName", asylumCase.read(AsylumCaseDefinition.APPELLANT_FAMILY_NAME, String.class).orElse(""))
--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/uppertribunal/UpperTribunalMarkAsReadyForUtTransferPersonalisationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/uppertribunal/UpperTribunalMarkAsReadyForUtTransferPersonalisationTest.java
@@ -27,6 +27,7 @@ public class UpperTribunalMarkAsReadyForUtTransferPersonalisationTest {
     private final String iaExUiFrontendUrl = "http://localhost";
     private final String emailAddress = "upperTribunal@example.com";
     private final String appealReferenceNumber = "someReferenceNumber";
+    private final String homeOfficeReferenceNumber = "someHomeOfficeReferenceNumber";
     private final String ariaListingReference = "someAriaListingReference";
     private final String appellantGivenNames = "someAppellantGivenNames";
     private final String appellantFamilyName = "someAppellantFamilyName";
@@ -44,6 +45,7 @@ public class UpperTribunalMarkAsReadyForUtTransferPersonalisationTest {
     public void setup() {
 
         when(asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class)).thenReturn(Optional.of(appealReferenceNumber));
+        when(asylumCase.read(HOME_OFFICE_REFERENCE_NUMBER, String.class)).thenReturn(Optional.of(homeOfficeReferenceNumber));
         when(asylumCase.read(ARIA_LISTING_REFERENCE, String.class)).thenReturn(Optional.of(ariaListingReference));
         when(asylumCase.read(APPELLANT_GIVEN_NAMES, String.class)).thenReturn(Optional.of(appellantGivenNames));
         when(asylumCase.read(APPELLANT_FAMILY_NAME, String.class)).thenReturn(Optional.of(appellantFamilyName));
@@ -101,6 +103,7 @@ public class UpperTribunalMarkAsReadyForUtTransferPersonalisationTest {
             upperTribunalMarkAsReadyForUtTransferPersonalisation.getPersonalisation(asylumCase);
 
         assertEquals(appealReferenceNumber, personalisation.get("appealReferenceNumber"));
+        assertEquals(homeOfficeReferenceNumber, personalisation.get("homeOfficeReferenceNumber"));
         assertEquals(ariaListingReference, personalisation.get("ariaListingReference"));
         assertEquals(appellantGivenNames, personalisation.get("appellantGivenNames"));
         assertEquals(appellantFamilyName, personalisation.get("appellantFamilyName"));
@@ -115,6 +118,7 @@ public class UpperTribunalMarkAsReadyForUtTransferPersonalisationTest {
     public void should_return_personalisation_when_all_mandatory_information_given() {
 
         when(asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class)).thenReturn(Optional.empty());
+        when(asylumCase.read(HOME_OFFICE_REFERENCE_NUMBER, String.class)).thenReturn(Optional.empty());
         when(asylumCase.read(ARIA_LISTING_REFERENCE, String.class)).thenReturn(Optional.empty());
         when(asylumCase.read(APPELLANT_GIVEN_NAMES, String.class)).thenReturn(Optional.empty());
         when(asylumCase.read(APPELLANT_FAMILY_NAME, String.class)).thenReturn(Optional.empty());
@@ -124,6 +128,7 @@ public class UpperTribunalMarkAsReadyForUtTransferPersonalisationTest {
             upperTribunalMarkAsReadyForUtTransferPersonalisation.getPersonalisation(asylumCase);
 
         assertEquals("", personalisation.get("appealReferenceNumber"));
+        assertEquals("", personalisation.get("homeOfficeReferenceNumber"));
         assertEquals("", personalisation.get("ariaListingReference"));
         assertEquals("", personalisation.get("appellantGivenNames"));
         assertEquals("", personalisation.get("appellantFamilyName"));
