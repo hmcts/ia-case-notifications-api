30da3e8d1795017be349686b361fcd9bd74874a2
Marcello Fabbri <56549133+Marcellofabbri@users.noreply.github.com>
2023-07-10 16:34:19 +0100

    RIA-7390 Make LR name be composite of first+last name (#900)
    * RIA-7390 Make LR name be composite of first+last name
    
    * RIA-7390 CVE suppressions

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/AsylumCaseDefinition.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/AsylumCaseDefinition.java
@@ -88,6 +88,9 @@ public enum AsylumCaseDefinition {
     LEGAL_REP_NAME(
         "legalRepName", new TypeReference<String>(){}),
 
+    LEGAL_REP_FAMILY_NAME(
+        "legalRepFamilyName", new TypeReference<String>(){}),
+
     LEGAL_REP_COMPANY_NAME(
             "legalRepCompanyName", new TypeReference<String>(){}),
 
