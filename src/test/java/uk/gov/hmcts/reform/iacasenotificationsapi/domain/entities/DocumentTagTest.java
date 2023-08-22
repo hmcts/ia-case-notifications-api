72afa1846f83efc185678c85391ed52e7cf25f08
Ivan Kirsanov <ivan.kirsanov@version1.com>
2023-08-22 17:35:59 +0200

    RIA-7373_mark_appeal_as_ada: Add test for new tag

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
@@ -46,7 +46,7 @@ public class DocumentTagTest {
         assertEquals("internalListCaseLetter", DocumentTag.INTERNAL_LIST_CASE_LETTER.toString());
         assertEquals("internalRequestHearingRequirementsLetter", DocumentTag.INTERNAL_REQUEST_HEARING_REQUIREMENTS_LETTER.toString());
         assertEquals("internalDetainedRequestHomeOfficeResponseReview", DocumentTag.INTERNAL_DETAINED_REQUEST_HO_RESPONSE_REVIEW.toString());
-        assertEquals("internalDetainedRequestHomeOfficeResponseReview", DocumentTag.INTERNAL_DET_MARK_AS_ADA_LETTER.toString());
+        assertEquals("internalDetMarkAsAdaLetter", DocumentTag.INTERNAL_DET_MARK_AS_ADA_LETTER.toString());
     }
 
     @Test
