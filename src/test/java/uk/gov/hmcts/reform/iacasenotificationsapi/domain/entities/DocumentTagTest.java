16252750d5fa1274bfccf787c7fe1a938d333f34
Ryan Nesbitt <rnesbitt03@qub.ac.uk>
2023-09-21 10:27:57 +0100

    RIA-7426 RIA-7692 Fixing unit test after rebase

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
@@ -65,6 +65,6 @@ class DocumentTagTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(58, DocumentTag.values().length);
+        assertEquals(59, DocumentTag.values().length);
     }
 }
