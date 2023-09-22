918d760e4e62e3801413c4754344fd697fac08c8
jamieclarkeHMCTS <128475668+jamieclarkeHMCTS@users.noreply.github.com>
2023-09-22 14:44:27 +0100

    RIA-7437 Updating document tag to match case-docs tag (#972)
    * RIA-7437 Updating document tag to match case-docs tag
    
    * RIA-7437 Updating document tag to match case-docs tag - test update
    
    * RIA-7437 Updating document tag to match case-docs tag - functional test update
    
    ---------
    
    Co-authored-by: JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTag.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTag.java
@@ -57,7 +57,7 @@ public enum DocumentTag {
     INTERNAL_DETAINED_TRANSFER_OUT_OF_ADA_LETTER("internalDetainedTransferOutOfAdaLetter"),
     INTERNAL_HO_FTPA_DECIDED_LETTER("internalHoFtpaDecidedLetter"),
     INTERNAL_APPELLANT_FTPA_DECIDED_LETTER("internalAppellantFtpaDecidedLetter"),
-    INTERNAL_NON_STANDARD_DIRECTION_APPELLANT_LETTER("internalNonStandardDirectionAppellantLetter"),
+    INTERNAL_NON_STANDARD_DIRECTION_TO_APPELLANT_LETTER("internalNonStandardDirectionToAppellantLetter"),
     INTERNAL_NON_STANDARD_DIRECTION_RESPONDENT_LETTER("internalNonStandardDirectionRespondentLetter"),
     INTERNAL_HEARING_REQUIREMENTS_UPDATED_LETTER("internalHearingRequirementsUpdatedLetter"),
     MAINTAIN_CASE_UNLINK_APPEAL_LETTER("maintainCaseUnlinkAppealLetter"),
