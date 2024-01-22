62c4ef41d47e73ae19831d2b1d6cd078daec3e40
JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>
2024-01-22 16:59:26 +0000

    RIA-8198 - checkstyle fixes

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/BailCaseUtils.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/BailCaseUtils.java
@@ -6,6 +6,11 @@ import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.BailCase;
 
 public class BailCaseUtils {
 
+    private BailCaseUtils() {
+        // private constructor to prevent sonar warning
+    }
+
+
     public static Boolean isBailGranted(BailCase bailCase) {
         if (bailCase.read(RECORD_DECISION_TYPE, String.class).orElse("").equals("granted")
             || bailCase.read(RECORD_DECISION_TYPE, String.class).orElse("").equals("conditionalGrant")) {
