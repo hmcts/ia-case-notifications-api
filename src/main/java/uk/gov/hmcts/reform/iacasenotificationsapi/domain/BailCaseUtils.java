2fca7562ee3c17679fef73cd3669d060b6ca3844
JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>
2024-01-22 16:45:41 +0000

    RIA-8198 - Decision Refused under IMA - Notification to UT

---
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/BailCaseUtils.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.iacasenotificationsapi.domain;
+
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.BailCaseFieldDefinition.RECORD_DECISION_TYPE;
+
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.BailCase;
+
+public class BailCaseUtils {
+
+    public static Boolean isBailGranted(BailCase bailCase) {
+        if (bailCase.read(RECORD_DECISION_TYPE, String.class).orElse("").equals("granted")
+            || bailCase.read(RECORD_DECISION_TYPE, String.class).orElse("").equals("conditionalGrant")) {
+            return true;
+        }
+        if (bailCase.read(RECORD_DECISION_TYPE, String.class).orElse("").equals("refused")
+            || bailCase.read(RECORD_DECISION_TYPE, String.class).orElse("").equals("refusedUnderIma")) {
+            return false;
+        }
+        return null;
+    }
+}
