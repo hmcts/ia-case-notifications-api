ba05004e4db0680ff091218927b02f26ddc4bc15
Miguel Martin <miguel.martin1@hmcts.net>
2024-02-28 13:28:00 +0100

    RIA-8053 Fix typo

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/RemissionOption.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/RemissionOption.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum RemissionOption {
 
-    ASYULM_SUPPORT_FROM_HOME_OFFICE("asylumSupportFromHo"),
+    ASYLUM_SUPPORT_FROM_HOME_OFFICE("asylumSupportFromHo"),
     FEE_WAIVER_FROM_HOME_OFFICE("feeWaiverFromHo"),
     UNDER_18_GET_SUPPORT("under18GetSupportFromLocalAuthority"),
     PARENT_GET_SUPPORT("parentGetSupportFromLocalAuthority"),
