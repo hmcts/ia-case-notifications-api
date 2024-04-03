38458c68f5ff5766dbe7de6201c55b6179b9ec3e
robin-ieong <robin.ieong@version1.com>
2024-04-03 12:05:55 +0100

    Cherry picked commits from 428a5ead 26/02/2024 to 2d19a472 15/03/2024

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/service/EmailNotificationGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/service/EmailNotificationGenerator.java
@@ -2,8 +2,10 @@ package uk.gov.hmcts.reform.iacasenotificationsapi.domain.service;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.ApplicationContextProvider;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
@@ -63,7 +65,7 @@ public class EmailNotificationGenerator implements NotificationGenerator {
                 emailNotificationPersonalisation,
                 referenceId,
                 callback)
-            ).toList();
+            ).filter(StringUtils::isNotBlank).collect(Collectors.toList());
     }
 
     protected String sendEmail(
