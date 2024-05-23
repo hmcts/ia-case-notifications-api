c415f419b9465b17049ddb52c049647361aba887
Miguel Martin <miguel.martin1@hmcts.net>
2024-05-23 11:55:28 +0200

    RIA-9037 fix unit test

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/ccd/EventTest.java
@@ -129,6 +129,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(118, Event.values().length);
+        assertEquals(119, Event.values().length);
     }
 }
