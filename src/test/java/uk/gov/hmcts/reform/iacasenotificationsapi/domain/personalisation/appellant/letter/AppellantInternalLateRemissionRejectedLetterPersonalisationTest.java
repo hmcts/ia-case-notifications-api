ce31e3602bab1bb78842ad10f74220c3b57c5b07
JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>
2024-06-24 09:19:11 +0100

    RIA-8720 - OOC updated

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/letter/AppellantInternalLateRemissionRejectedLetterPersonalisationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/letter/AppellantInternalLateRemissionRejectedLetterPersonalisationTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDate;
 import java.util.Map;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
