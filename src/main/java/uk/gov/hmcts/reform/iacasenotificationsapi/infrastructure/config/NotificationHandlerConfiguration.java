d7e8b747f49f8b232374f2e9c3d2ce30393b7c25
nilay <nilay.patel@hmcts.net>
2024-08-21 10:49:32 +0100

    DIAC-546 making sms stop representing presubmit

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -3713,16 +3713,14 @@ public class NotificationHandlerConfiguration {
     }
 
     @Bean
-    public PostSubmitCallbackHandler<AsylumCase> removeRepresentativeAppellantSmsNotificationHandler(
+    public PreSubmitCallbackHandler<AsylumCase> removeRepresentativeAppellantSmsNotificationHandler(
         @Qualifier("removeRepresentativeAppellantSmsNotificationGenerator")
             List<NotificationGenerator> notificationGenerators) {
 
-        return new PostSubmitNotificationHandler(
-            (callbackStage, callback) -> {
-                return callbackStage == PostSubmitCallbackStage.CCD_SUBMITTED
-                       && callback.getEvent() == Event.REMOVE_LEGAL_REPRESENTATIVE
-                       && callback.getCaseDetails().getCaseData().read(MOBILE_NUMBER, String.class).isPresent();
-            },
+        return new NotificationHandler(
+            (callbackStage, callback) -> callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+                && callback.getEvent() == Event.REMOVE_LEGAL_REPRESENTATIVE
+                && callback.getCaseDetails().getCaseData().read(MOBILE_NUMBER, String.class).isPresent(),
             notificationGenerators
         );
     }
