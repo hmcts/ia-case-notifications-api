0787950f9e89cdce3a08437e9e63d870e1f495da
tiagreaney <tiagreaney@gmail.com>
2025-01-29 09:06:32 +0000

    revert review direction lr and appellant

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/email/AppellantRespondentReviewPersonalisationEmail.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/email/AppellantRespondentReviewPersonalisationEmail.java
@@ -1,18 +1,15 @@
 package uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.appellant.email;
 
 import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.utils.AsylumCaseUtils.isAcceleratedDetainedAppeal;
 
 import com.google.common.collect.ImmutableMap;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.*;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.EmailNotificationPersonalisation;
-import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.DirectionFinder;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.RecipientsFinder;
 import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.CustomerServicesProvider;
 
@@ -23,7 +20,6 @@ public class AppellantRespondentReviewPersonalisationEmail implements EmailNotif
     private final String requestRespondentReviewAppellantEmailTemplateId;
     private final String iaAipFrontendUrl;
     private final RecipientsFinder recipientsFinder;
-    private final DirectionFinder directionFinder;
     private final CustomerServicesProvider customerServicesProvider;
 
     @Value("${govnotify.emailPrefix.ada}")
@@ -35,12 +31,11 @@ public class AppellantRespondentReviewPersonalisationEmail implements EmailNotif
             @Value("${govnotify.template.reviewDirection.appellant.email}") String requestRespondentReviewAppellantEmailTemplateId,
             @Value("${iaAipFrontendUrl}") String iaAipFrontendUrl,
             RecipientsFinder recipientsFinder,
-            DirectionFinder directionFinder, CustomerServicesProvider customerServicesProvider) {
+            CustomerServicesProvider customerServicesProvider) {
 
         this.requestRespondentReviewAppellantEmailTemplateId = requestRespondentReviewAppellantEmailTemplateId;
         this.iaAipFrontendUrl = iaAipFrontendUrl;
         this.recipientsFinder = recipientsFinder;
-        this.directionFinder = directionFinder;
         this.customerServicesProvider = customerServicesProvider;
     }
 
@@ -63,44 +58,15 @@ public class AppellantRespondentReviewPersonalisationEmail implements EmailNotif
     public Map<String, String> getPersonalisation(AsylumCase asylumCase) {
         requireNonNull(asylumCase, "asylumCase must not be null");
 
-        final Direction direction =
-                directionFinder
-                        .findFirst(asylumCase, DirectionTag.RESPONDENT_REVIEW)
-                        .orElseThrow(() -> new IllegalStateException("Appellant respondent review is not present"));
-
-
-        final String directionDueDate =
-                LocalDate
-                        .parse(direction.getDateDue())
-                        .format(DateTimeFormatter.ofPattern("d MMM yyyy"));
-
         return ImmutableMap
             .<String, String>builder()
             .putAll(customerServicesProvider.getCustomerServicesPersonalisation())
+            .put("subjectPrefix", isAcceleratedDetainedAppeal(asylumCase) ? adaPrefix : nonAdaPrefix)
             .put("appealReferenceNumber", asylumCase.read(AsylumCaseDefinition.APPEAL_REFERENCE_NUMBER, String.class).orElse(""))
             .put("homeOfficeReferenceNumber", asylumCase.read(AsylumCaseDefinition.HOME_OFFICE_REFERENCE_NUMBER, String.class).orElse(""))
             .put("appellantGivenNames", asylumCase.read(AsylumCaseDefinition.APPELLANT_GIVEN_NAMES, String.class).orElse(""))
             .put("appellantFamilyName", asylumCase.read(AsylumCaseDefinition.APPELLANT_FAMILY_NAME, String.class).orElse(""))
-            .put("dueDate", directionDueDate)
-            .put("HearingCentre", getHearingCentreName(asylumCase))
-                .build();
-    }
-
-    private String getHearingCentreName(AsylumCase caseData) {
-
-        HearingCentre hearingCentre = caseData.read(AsylumCaseDefinition.HEARING_CENTRE, HearingCentre.class)
-                .orElseThrow(() -> new IllegalStateException("hearingCentre is not present"));
-        String hearingCentreName = String.valueOf(hearingCentre);
-
-        String[] words = hearingCentreName.replaceAll("([a-z])([A-Z])", "$1 $2").split(" ");
-        StringBuilder formattedName = new StringBuilder();
-
-        for (String word : words) {
-            formattedName.append(word.substring(0, 1).toUpperCase())
-                    .append(word.substring(1).toLowerCase())
-                    .append(" ");
-        }
-
-        return formattedName.toString().trim();
+            .put("Hyperlink to service", iaAipFrontendUrl)
+            .build();
     }
 }
