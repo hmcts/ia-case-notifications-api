e71ea1d8cf942be1c90f2a43549199ebfa9c89db
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2025-02-03 17:35:40 +0000

    DIAC-859 HomeOffice notification for AiP remission appeals

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationGeneratorConfiguration.java
@@ -5433,13 +5433,14 @@ public class NotificationGeneratorConfiguration {
     public List<NotificationGenerator> appellantSubmittedWithRemissionRequestNotificationGenerator(
         AppellantSubmittedWithRemissionRequestPersonalisationEmail appellantSubmittedWithRemissionRequestPersonalisationEmail,
         AppellantSubmittedWithRemissionRequestPersonalisationSms appellantSubmittedWithRemissionRequestPersonalisationSms,
+        HomeOfficeSubmitAppealPersonalisation homeOfficeSubmitAppealPersonalisation,
         GovNotifyNotificationSender notificationSender,
         NotificationIdAppender notificationIdAppender
     ) {
 
         return Arrays.asList(
             new EmailNotificationGenerator(
-                newArrayList(appellantSubmittedWithRemissionRequestPersonalisationEmail),
+                newArrayList(appellantSubmittedWithRemissionRequestPersonalisationEmail, homeOfficeSubmitAppealPersonalisation),
                 notificationSender,
                 notificationIdAppender
             ),
