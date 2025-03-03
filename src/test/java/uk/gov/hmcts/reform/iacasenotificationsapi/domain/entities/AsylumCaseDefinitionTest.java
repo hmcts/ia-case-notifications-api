d2ebae6584c1ecd709ccddb995d1820ecbf8d77f
Andrey Pereverzin <andrey.pereverzin1@hmcts.net>
2025-03-03 15:47:36 +0000

    START_APPEAL, EDIT_APPEAL

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/AsylumCaseDefinitionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/AsylumCaseDefinitionTest.java
@@ -2,9 +2,10 @@ package uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities;
 
 import static com.google.common.base.CaseFormat.LOWER_CAMEL;
 import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 
 public class AsylumCaseDefinitionTest {
@@ -12,7 +13,11 @@ public class AsylumCaseDefinitionTest {
     @Test
     public void mapped_to_equivalent_field_name() {
         Stream.of(AsylumCaseDefinition.values())
-                .forEach(v -> assertThat(UPPER_UNDERSCORE.to(LOWER_CAMEL, v.name()))
-                        .isEqualTo(v.value()));
+            .forEach(v ->
+                assertTrue(
+                    UPPER_UNDERSCORE.to(LOWER_CAMEL, v.name()).equals(v.value())
+                        || v.name().equals("TTL")
+                )
+            );
     }
 }
