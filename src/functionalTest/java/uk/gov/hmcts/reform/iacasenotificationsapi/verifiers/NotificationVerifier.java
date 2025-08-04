5e6c0822fcb2a6409f0159ab063e06033242d87c
Andrey Pereverzin <andreypereverzin@Mac.broadband>
2025-08-04 20:17:17 +0100

    DIAC-1341

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasenotificationsapi/verifiers/NotificationVerifier.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasenotificationsapi/verifiers/NotificationVerifier.java
@@ -3,19 +3,13 @@ package uk.gov.hmcts.reform.iacasenotificationsapi.verifiers;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.google.common.base.Strings;
-
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.clients.RetryableNotificationClient;
@@ -31,133 +25,127 @@ public class NotificationVerifier implements Verifier {
     private RetryableNotificationClient notificationClient;
 
     public void verify(
-        long testCaseId,
-        Map<String, Object> scenario,
-        Map<String, Object> expectedResponse,
-        Map<String, Object> actualResponse
+            long testCaseId,
+            Map<String, Object> scenario,
+            Map<String, Object> expectedResponse,
+            Map<String, Object> actualResponse
     ) {
         String description = MapValueExtractor.extract(scenario, "description");
 
         List<Map<String, Object>> expectedNotifications =
-            MapValueExtractor.extractOrDefault(scenario, "expectation.notifications", Collections.emptyList());
+                MapValueExtractor.extractOrDefault(scenario, "expectation.notifications", Collections.emptyList());
 
-        if (expectedNotifications == null || expectedNotifications.isEmpty()) {
+        if (expectedNotifications == null
+                || expectedNotifications.size() == 0) {
             return;
         }
 
         List<Map<String, Object>> notificationsSent =
-            MapValueExtractor.extractOrDefault(actualResponse, "data.notificationsSent", Collections.emptyList());
+                MapValueExtractor.extractOrDefault(actualResponse, "data.notificationsSent", Collections.emptyList());
 
         if (notificationsSent.isEmpty()) {
             assertFalse(
-                notificationsSent.isEmpty(),
-                description + ": Notifications were not delivered"
+                    notificationsSent.isEmpty(),
+                    description + ": Notifications were not delivered"
             );
         }
 
         Map<String, String> notificationsSentMap =
-            notificationsSent
-                .stream()
-                .collect(Collectors.toMap(
-                    notificationSent -> sanitizeNotificationId((String) notificationSent.get("id")),
-                    notificationSent -> (String) notificationSent.get("value")
-                ));
+                notificationsSent
+                        .stream()
+                        .collect(Collectors.toMap(
+                                notificationSent -> (String) notificationSent.get("id"),
+                                notificationSent -> (String) notificationSent.get("value")
+                        ));
 
         expectedNotifications
-            .forEach(expectedNotification -> {
+                .forEach(expectedNotification -> {
 
-                final String expectedReference = MapValueExtractor.extractOrThrow(expectedNotification, "reference");
-                final String expectedRecipient = MapValueExtractor.extractOrThrow(expectedNotification, "recipient");
-                final String expectedSubject = MapValueExtractor.extractOrThrow(expectedNotification, "subject");
-                final Object expectedBodyUnknownType = MapValueExtractor.extractOrThrow(expectedNotification, "body");
+                    final String expectedReference = MapValueExtractor.extractOrThrow(expectedNotification, "reference");
+                    final String expectedRecipient = MapValueExtractor.extractOrThrow(expectedNotification, "recipient");
+                    final String expectedSubject = MapValueExtractor.extractOrThrow(expectedNotification, "subject");
+                    final Object expectedBodyUnknownType = MapValueExtractor.extractOrThrow(expectedNotification, "body");
 
-                final String deliveredNotificationId = notificationsSentMap.get(expectedReference);
-                if (Strings.isNullOrEmpty(deliveredNotificationId)) {
-                    assertFalse(
-                        true,
-                        description
-                            + ": Notification " + expectedReference + " was not delivered successfully"
-                    );
-                }
-
-                try {
-
-                    Notification notification = notificationClient.getNotificationById(deliveredNotificationId);
-
-                    final String actualReference = sanitizeNotificationId(notification.getReference().orElse(""));
-                    final String actualRecipient =
-                        notification.getEmailAddress().orElse(notification.getPhoneNumber().orElse(""));
-                    final String actualSubject = notification.getSubject().orElse("");
-                    final String actualBody = notification.getBody();
-
-                    assertThat(
-                        description
-                            + ": Notification "
-                            + expectedReference
-                            + " was delivered with wrong reference",
-                        actualReference,
-                        equalTo(expectedReference)
-                    );
-
-                    assertEquals(
-                            expectedRecipient,
-                            actualRecipient,
-                            "Notification was delivered to wrong recipient|"
-                                    + Arrays.toString(expectedRecipient.getBytes())
-                                    + "|"
-                                    + Arrays.toString(actualRecipient.getBytes())
-                    );
-
-                    assertThat(
-                        description
-                            + ": Notification "
-                            + expectedReference
-                            + " was delivered with wrong subject content",
-                        actualSubject,
-                        equalTo(expectedSubject)
-                    );
-
-                    if (expectedBodyUnknownType instanceof String) {
-
-                        assertThat(
-                            description
-                                + ": Notification "
-                                + expectedReference
-                                + " was delivered with wrong body content",
-                            actualBody,
-                            equalTo((String) expectedBodyUnknownType)
-                        );
-
-                    } else {
-
-                        List<String> expectedBodyMatches = (List<String>) expectedBodyUnknownType;
-
-                        expectedBodyMatches.forEach(expectedBodyMatch -> {
-
-                            assertThat(
+                    final String deliveredNotificationId = notificationsSentMap.get(expectedReference);
+                    if (Strings.isNullOrEmpty(deliveredNotificationId)) {
+                        assertFalse(
+                                true,
                                 description
-                                    + ": Notification "
-                                    + expectedReference
-                                    + " was delivered with wrong body content match",
-                                actualBody,
-                                containsString(expectedBodyMatch)
-                            );
-                        });
+                                        + ": Notification " + expectedReference + " was not delivered successfully"
+                        );
                     }
 
-                } catch (NotificationClientException e) {
-                    assertFalse(
-                        true,
-                        description + ": Notification " + deliveredNotificationId + " was not found on GovNotify"
-                    );
-                }
-            });
-    }
+                    try {
 
-    private String sanitizeNotificationId(String notificationIdWithTimestamp) {
-        // Regular expression to remove the last underscore and following timestamp in epochmillis
-        Pattern pattern = Pattern.compile("^(.*)_\\d{13}$");
-        Matcher matcher = pattern.matcher(notificationIdWithTimestamp);
-        return matcher.find() ? matcher.group(1) : notificationIdWithTimestamp;
+                        Notification notification = notificationClient.getNotificationById(deliveredNotificationId);
+
+                        final String actualReference = notification.getReference().orElse("");
+                        final String actualRecipient =
+                                notification.getEmailAddress().orElse(notification.getPhoneNumber().orElse(""));
+                        final String actualSubject = notification.getSubject().orElse("");
+                        final String actualBody = notification.getBody();
+
+                        assertThat(
+                                description
+                                        + ": Notification "
+                                        + expectedReference
+                                        + " was delivered with wrong reference",
+                                actualReference,
+                                equalTo(expectedReference)
+                        );
+
+                        assertThat(
+                                description
+                                        + ": Notification "
+                                        + expectedReference
+                                        + " was delivered to wrong recipient",
+                                actualRecipient,
+                                equalTo(expectedRecipient)
+                        );
+
+                        assertThat(
+                                description
+                                        + ": Notification "
+                                        + expectedReference
+                                        + " was delivered with wrong subject content",
+                                actualSubject,
+                                equalTo(expectedSubject)
+                        );
+
+                        if (expectedBodyUnknownType instanceof String) {
+
+                            assertThat(
+                                    description
+                                            + ": Notification "
+                                            + expectedReference
+                                            + " was delivered with wrong body content",
+                                    actualBody,
+                                    equalTo((String) expectedBodyUnknownType)
+                            );
+
+                        } else {
+
+                            List<String> expectedBodyMatches = (List<String>) expectedBodyUnknownType;
+
+                            expectedBodyMatches.forEach(expectedBodyMatch -> {
+
+                                assertThat(
+                                        description
+                                                + ": Notification "
+                                                + expectedReference
+                                                + " was delivered with wrong body content match",
+                                        actualBody,
+                                        containsString(expectedBodyMatch)
+                                );
+                            });
+                        }
+
+                    } catch (NotificationClientException e) {
+                        assertFalse(
+                                true,
+                                description + ": Notification " + deliveredNotificationId + " was not found on GovNotify"
+                        );
+                    }
+                });
     }
 }
