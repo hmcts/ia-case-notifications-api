652f8a17439032b982ea8a38b4774bb0ed527735
Mike Brown <michael.brown@hmcts.net>
2025-08-06 11:04:53 +0100

    logic in NotificationHandlerConfiguration corrected, FT name corrected

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -6320,7 +6320,6 @@ public class NotificationHandlerConfiguration {
                 return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                        && callback.getEvent() == REQUEST_RESPONDENT_EVIDENCE
                        && isInternalCase(asylumCase)
-                       && !isAppellantInDetention(asylumCase)
                        && ((!isAppellantInDetention(asylumCase)) || isDetainedInFacilityType(asylumCase, OTHER));
 
             },
