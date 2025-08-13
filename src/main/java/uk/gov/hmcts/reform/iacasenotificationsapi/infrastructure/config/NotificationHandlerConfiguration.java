aded397aed4a3cb734d0856c7b56eef4b4365c56
Andrey Pereverzin <andreypereverzin@Mac.broadband>
2025-08-13 16:29:04 +0100

    DIAC-1357 FTPA submitted by HO  - Detained (in Other) notification

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -6231,7 +6231,13 @@ public class NotificationHandlerConfiguration {
                 return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                     && callback.getEvent() == APPLY_FOR_FTPA_RESPONDENT
                     && isInternalCase(asylumCase)
-                    && (!isAppellantInDetention(asylumCase) || !inCountryAppeal(asylumCase));
+                    && (
+                        !isAppellantInDetention(asylumCase)
+                        ||
+                        !inCountryAppeal(asylumCase)
+                        ||
+                        isAppellantInDetention(asylumCase) && isDetainedInFacilityType(asylumCase, OTHER)
+                    );
 
             },
             notificationGenerators,
