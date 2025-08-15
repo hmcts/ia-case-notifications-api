dedb3ff27bcc77ecc4f5ad10183de51c889a55a3
Mike Brown <michael.brown@hmcts.net>
2025-08-15 14:39:48 +0100

    import removed

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/AsylumCaseUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/AsylumCaseUtilsTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.iacasenotificationsapi.domain;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
