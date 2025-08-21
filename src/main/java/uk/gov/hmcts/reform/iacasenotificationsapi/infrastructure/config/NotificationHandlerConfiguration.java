99b087cd044a8d625ee0ad230bb9ab2480e8e14e
erclaudio <claudio.andrei@icloud.com>
2025-08-21 17:03:11 +0100

    added detained flow

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -6826,7 +6826,7 @@ public class NotificationHandlerConfiguration {
                 return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                        && callback.getEvent() == RECORD_REMISSION_DECISION
                        && isInternalCase(asylumCase)
-                       && !isAppellantInDetention(asylumCase)
+                       && !isAppellantInDetention(asylumCase) || isDetainedInFacilityType(asylumCase, OTHER)
                        && isApproved
                        && !isOutOfTimeAppeal
                        && lateRemissionType.isEmpty();
