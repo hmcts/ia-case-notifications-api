5e1212315286889078b5540a343309a2787872cb
tonym <129744569+tonymort@users.noreply.github.com>
2025-09-02 15:30:34 +0100

    Squashed commits from DIAC-1311 (#1462)

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationGeneratorConfiguration.java
@@ -4045,6 +4045,21 @@ public class NotificationGeneratorConfiguration {
         );
     }
 
+    @Bean("appealSubmittedLateWithExemptionEmailInternalNotificationGenerator")
+    public List<NotificationGenerator> appealSubmittedLateWithExemptionEmailInternalNotificationHandler(
+        DetentionEngagementTeamAppealSubmittedLateWithExemptionPersonalisation appealSubmittedLateWithExemptionPersonalisation,
+        GovNotifyNotificationSender notificationSender,
+        NotificationIdAppender notificationIdAppender) {
+
+        return Arrays.asList(
+            new EmailWithLinkNotificationGenerator(
+                newArrayList(Collections.singleton(appealSubmittedLateWithExemptionPersonalisation)),
+                notificationSender,
+                notificationIdAppender
+            )
+        );
+    }
+
     @Bean("recordOfTimeDecisionCannotProceedEmailNotificationGenerator")
     public List<NotificationGenerator> recordOfTimeDecisionCannotProceedEmailNotificationHandler(
         LegalRepresentativeRecordOutOfTimeDecisionCannotProceed legalRepresentativeRecordOutOfTimeDecisionCannotProceed,
