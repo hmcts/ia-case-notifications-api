59c5cd2bdd0e0f658c23c64c13e2bf8c38baffc9
erclaudio <claudio.andrei@icloud.com>
2025-09-16 14:00:45 +0100

    :test

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
@@ -89,6 +89,6 @@ class DocumentTagTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(82, DocumentTag.values().length);
+        assertEquals(83, DocumentTag.values().length);
     }
 }
