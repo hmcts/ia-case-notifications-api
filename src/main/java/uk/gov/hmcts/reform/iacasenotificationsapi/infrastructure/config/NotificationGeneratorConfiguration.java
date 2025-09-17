ab90d97f37be5f9f7dbd0d2a658d66926101d6f0
erclaudio <claudio.andrei@icloud.com>
2025-09-17 15:44:07 +0100

    fix failing scenarios by adding the removed notification setup from generator

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationGeneratorConfiguration.java
@@ -1066,10 +1066,15 @@ public class NotificationGeneratorConfiguration {
     @Bean("respondentReviewInternalNotificationGenerator")
     public List<NotificationGenerator> respondentReviewInternalNotificationGenerator(
         RespondentDirectionPersonalisation respondentDirectionPersonalisation,
+        DetentionEngagementTeamRespondentReviewPersonalisation detentionEngagementTeamRespondentReviewPersonalisation,
         GovNotifyNotificationSender notificationSender,
         NotificationIdAppender notificationIdAppender) {
 
-        return singletonList(
+        return Arrays.asList(
+            new EmailWithLinkNotificationGenerator(
+                newArrayList(detentionEngagementTeamRespondentReviewPersonalisation),
+                notificationSender,
+                notificationIdAppender),
 
             new EmailNotificationGenerator(
                 newArrayList(respondentDirectionPersonalisation),
