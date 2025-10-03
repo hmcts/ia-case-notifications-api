6e2d9ef0a82fb7b64da6e14a1c890cc4d6657fa5
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2025-10-03 10:46:35 +0100

    Update DocumentTagTest.java

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
@@ -94,6 +94,6 @@ class DocumentTagTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(87, DocumentTag.values().length);
+        assertEquals(88, DocumentTag.values().length);
     }
 }
