8df08472b9482d12da4b391094f0c40db359f971
Mike Brown <michael.brown@hmcts.net>
2025-10-10 16:41:47 +0100

    test count bumped

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
@@ -99,6 +99,6 @@ class DocumentTagTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(93, DocumentTag.values().length);
+        assertEquals(94, DocumentTag.values().length);
     }
 }
