77c30b9a8b887d1193404a708d9757d750e39858
erclaudio <claudio.andrei@icloud.com>
2025-10-13 13:27:48 +0100

    remove duplicate check for inernal case

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -2308,9 +2308,8 @@ public class NotificationHandlerConfiguration {
             (callbackStage, callback) ->
                 callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                     && callback.getEvent() == Event.APPLY_FOR_FTPA_APPELLANT
-                    && isInternalCase(callback.getCaseDetails().getCaseData())
-                    && hasBeenSubmittedByAppellantInternalCase(callback.getCaseDetails().getCaseData()),
-            //                    && !isDetainedInFacilityType(callback.getCaseDetails().getCaseData(), OTHER),
+                    && hasBeenSubmittedByAppellantInternalCase(callback.getCaseDetails().getCaseData())
+                    && !isDetainedInFacilityType(callback.getCaseDetails().getCaseData(), OTHER),
             notificationGenerator
         );
     }
