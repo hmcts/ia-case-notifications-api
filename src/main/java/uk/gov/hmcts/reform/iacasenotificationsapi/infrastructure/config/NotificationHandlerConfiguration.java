41f31ea284b76bdcb7a71b1c06a0fc1148fdde6a
erclaudio <claudio.andrei@icloud.com>
2025-10-13 10:44:27 +0100

    reverting checks

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -2308,9 +2308,9 @@ public class NotificationHandlerConfiguration {
             (callbackStage, callback) ->
                 callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                     && callback.getEvent() == Event.APPLY_FOR_FTPA_APPELLANT
-                    && isInternalCase(callback.getCaseDetails().getCaseData())
-                    && hasBeenSubmittedByAppellantInternalCase(callback.getCaseDetails().getCaseData())
-                    && !isDetainedInFacilityType(callback.getCaseDetails().getCaseData(), OTHER),
+                    && isInternalCase(callback.getCaseDetails().getCaseData()),
+//                    && hasBeenSubmittedByAppellantInternalCase(callback.getCaseDetails().getCaseData())
+//                    && !isDetainedInFacilityType(callback.getCaseDetails().getCaseData(), OTHER),
             notificationGenerator
         );
     }
