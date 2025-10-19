c0db9ac2c66d2c71bb3c1d39739fb742e9c3297d
Mike Brown <michael.brown@hmcts.net>
2025-10-19 12:28:27 +0100

    unit tests fixed

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/AppealOutcomeAdminNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/AppealOutcomeAdminNotificationHandlerTest.java
@@ -9,17 +9,23 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.NotificationGenerator;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Collections.emptyList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.APPELLANTS_REPRESENTATION;
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.IS_ADMIN;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.Event.SEND_DECISION_AND_REASONS;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
@@ -32,12 +38,22 @@ class AppealOutcomeAdminNotificationHandlerTest {
     @Mock
     private Callback<AsylumCase> callback;
 
+    @Mock
+    private AsylumCase asylumCase;
+
+    @Mock
+    private CaseDetails<AsylumCase> caseDetails;
+
     @BeforeEach
     void setUp() {
         NotificationHandlerConfiguration notificationHandlerConfiguration = new NotificationHandlerConfiguration();
         List<NotificationGenerator> notificationGenerators = emptyList();
         asylumCasePreSubmitCallbackHandler =
                 notificationHandlerConfiguration.appealOutcomeAdminNotificationHandler(notificationGenerators);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(asylumCase.read(IS_ADMIN, YesOrNo.class)).thenReturn(Optional.of(YesOrNo.NO));
+        when(asylumCase.read(APPELLANTS_REPRESENTATION, YesOrNo.class)).thenReturn(Optional.of(YesOrNo.NO));
     }
 
     @ParameterizedTest
