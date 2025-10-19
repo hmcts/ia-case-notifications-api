96ba70529e2586fb32fc8bc6468c2e2623320097
KleoG <KleoG@users.noreply.github.com>
2025-10-19 12:42:11 +0100

    DIAC-1647 - admin notifications for submit appeal payments (#1575)
    * DIAC-1647 - enable internal notifications for list case and edit case listing
    
    * DIAC-1647 - call the same qualifier (relist case) for edit case listing - for admin notifications
    
    * DIAC-1647 - edit case listing for internal cases
    
    * DIAC-1647 - checkstyle fix
    
    * DIAC-1647 - fix functional tests
    
    * DIAC-1647 - fix functional tests
    
    * DIAC-1647 - update submitappeal payments to be for payLater and not payOffline as it is no longer an option
    
    * DIAC-1647 - keep payOffline for backwards compatibility

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -3412,7 +3412,7 @@ public class NotificationHandlerConfiguration {
                     && callback.getEvent() == Event.SUBMIT_APPEAL
                     && isCorrectAppealType
                     && !isInternalCase(asylumCase)
-                    && (paPaymentOption.equals("payOffline") || isRemissionOptedForEaOrHuOrPaAppeal(callback));
+                    && ((paPaymentOption.equals("payLater") || paPaymentOption.equals("payOffline")) || isRemissionOptedForEaOrHuOrPaAppeal(callback));
             }, notificationGenerators
         );
     }
@@ -3436,7 +3436,7 @@ public class NotificationHandlerConfiguration {
                     && callback.getEvent() == Event.SUBMIT_APPEAL
                     && isInternalCase(asylumCase)
                     && isCorrectAppealType
-                    && (paPaymentOption.equals("payOffline") || isRemissionOptedForEaOrHuOrPaAppeal(callback));
+                    && ((paPaymentOption.equals("payLater") || paPaymentOption.equals("payOffline")) || isRemissionOptedForEaOrHuOrPaAppeal(callback));
             }, notificationGenerators
         );
     }
