33404c6958bfb58310b4ba77cd68ac974f821bc8
Sabah Irfan <sabah.irfan@gmail.com>
2025-10-23 15:58:37 +0100

    DIAC-1766 System is trying to send a notification to Legal Representative for an Appellant in Person appeal

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -2585,7 +2585,6 @@ public class NotificationHandlerConfiguration {
             (callbackStage, callback) ->
                 callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                 && callback.getEvent() == Event.FORCE_CASE_TO_SUBMIT_HEARING_REQUIREMENTS
-                && isAppellantInDetention(callback.getCaseDetails().getCaseData())
                 && !isInternalCase(callback.getCaseDetails().getCaseData()),
             notificationGenerator
         );
