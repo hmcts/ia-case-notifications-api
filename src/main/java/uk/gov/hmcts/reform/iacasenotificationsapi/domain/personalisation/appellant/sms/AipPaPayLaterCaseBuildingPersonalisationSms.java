145288978252a6a3c7596a88cba50d0c17af68f1
tiagreaney <tiagreaney@gmail.com>
2026-01-20 15:22:26 +0000

    checkstyle fix

---
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/sms/AipPaPayLaterCaseBuildingPersonalisationSms.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.appellant.sms;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.*;
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.utils.CommonUtils.convertAsylumCaseFeeValue;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.NotificationType;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.SmsNotificationPersonalisation;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.RecipientsFinder;
+
+@Service
+public class AipPaPayLaterCaseBuildingPersonalisationSms implements SmsNotificationPersonalisation {
+
+    private final String paPayLaterCaseBuildingTemplateId;
+    private final RecipientsFinder recipientsFinder;
+    private final String iaAipFrontendUrl;
+
+    public AipPaPayLaterCaseBuildingPersonalisationSms(
+            @Value("${govnotify.template.caseBuilding.paPayLater.sms}") String paPayLaterCaseBuildingTemplateId,
+            @Value("${iaAipFrontendUrl}") String iaAipFrontendUrl, RecipientsFinder recipientsFinder
+    ) {
+        this.paPayLaterCaseBuildingTemplateId = paPayLaterCaseBuildingTemplateId;
+        this.iaAipFrontendUrl = iaAipFrontendUrl;
+        this.recipientsFinder = recipientsFinder;
+    }
+
+    @Override
+    public String getTemplateId(AsylumCase asylumCase) {
+        return paPayLaterCaseBuildingTemplateId;
+    }
+
+    @Override
+    public Set<String> getRecipientsList(final AsylumCase asylumCase) {
+        return recipientsFinder.findAll(asylumCase, NotificationType.SMS);
+    }
+
+    @Override
+    public String getReferenceId(Long caseId) {
+        return caseId + "_AIP_PA_PAY_LATER_CASE_BUILDING_SMS";
+    }
+
+    @Override
+    public Map<String, String> getPersonalisation(AsylumCase asylumCase) {
+        requireNonNull(asylumCase, "asylumCase must not be null");
+
+        return ImmutableMap
+                .<String, String>builder()
+                .put("appealReferenceNumber", asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class).orElse(""))
+                .put("linkToService", iaAipFrontendUrl)
+                .build();
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/sms/AipPaPayLaterListingPersonalisationSms.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.appellant.sms;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.*;
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.utils.CommonUtils.convertAsylumCaseFeeValue;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.NotificationType;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.SmsNotificationPersonalisation;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.RecipientsFinder;
+
+@Service
+public class AipPaPayLaterListingPersonalisationSms implements SmsNotificationPersonalisation {
+
+    private final String paPayLaterListingTemplateId;
+    private final RecipientsFinder recipientsFinder;
+    private final String iaAipFrontendUrl;
+
+    public AipPaPayLaterListingPersonalisationSms(
+            @Value("${govnotify.template.listing.paPayLater.sms}") String paPayLaterListingTemplateId,
+            @Value("${iaAipFrontendUrl}") String iaAipFrontendUrl, RecipientsFinder recipientsFinder
+    ) {
+        this.paPayLaterListingTemplateId = paPayLaterListingTemplateId;
+        this.iaAipFrontendUrl = iaAipFrontendUrl;
+        this.recipientsFinder = recipientsFinder;
+    }
+
+    @Override
+    public String getTemplateId(AsylumCase asylumCase) {
+        return paPayLaterListingTemplateId;
+    }
+
+    @Override
+    public Set<String> getRecipientsList(final AsylumCase asylumCase) {
+        return recipientsFinder.findAll(asylumCase, NotificationType.SMS);
+    }
+
+    @Override
+    public String getReferenceId(Long caseId) {
+        return caseId + "_AIP_PA_PAY_LATER_CASE_LISTING_SMS";
+    }
+
+    @Override
+    public Map<String, String> getPersonalisation(AsylumCase asylumCase) {
+        requireNonNull(asylumCase, "asylumCase must not be null");
+
+        return ImmutableMap
+                .<String, String>builder()
+                .put("appealReferenceNumber", asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class).orElse(""))
+                .put("linkToService", iaAipFrontendUrl)
+                .build();
+    }
+}
