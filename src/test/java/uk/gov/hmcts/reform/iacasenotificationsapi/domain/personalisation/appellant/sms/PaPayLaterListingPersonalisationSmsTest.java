a307b8caae360045ec68acd6099faee3b2864a86
tiagreaney <tiagreaney@gmail.com>
2026-01-22 12:56:32 +0000

    update sms personalisation

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/sms/PaPayLaterListingPersonalisationSmsTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/appellant/sms/PaPayLaterListingPersonalisationSmsTest.java
@@ -1,51 +1,72 @@
-package uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.appellant.sms;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.*;
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.APPEAL_REFERENCE_NUMBER;
 
 import java.util.Map;
 import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
+
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.RecipientsFinder;
+import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.SystemDateProvider;
 
 @ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
 class PaPayLaterListingPersonalisationSmsTest {
 
     @Mock
-    AsylumCase asylumCase;
+    private AsylumCase asylumCase;
+
     @Mock
-    RecipientsFinder recipientsFinder;
-    private Long caseId = 12345L;
-    private String paPayLaterListingTemplateId = "paPayLaterListingTemplateId";
-    private String iaAipFrontendUrl = "http://localhost";
-    private String mockedAppealReferenceNumber = "someReferenceNumber";
-    private String someTestDateSms = "14/14/2024";
-    private PaPayLaterListingPersonalisationSms paPayLaterListingPersonalisationSms;
+    private Callback<AsylumCase> callback;
+
+    @Mock
+    private CaseDetails<AsylumCase> caseDetails;
+
+    @Mock
+    private RecipientsFinder recipientsFinder;
+
+    @Mock
+    private SystemDateProvider systemDateProvider;
+
+    private PaPayLaterListingPersonalisationSms personalisation;
+
+    private final Long caseId = 12345L;
+    private final String templateId = "PaPayLaterListingTemplateId";
+    private final String iaAipFrontendUrl = "http://localhost";
+    private final int daysAfterNotificationSent = 14;
+    private final String mockedAppealReferenceNumber = "someReferenceNumber";
+    private final String mockedDueDate = "14/04/2024";
 
     @BeforeEach
-    public void setup() {
+    void setup() {
 
-        paPayLaterListingPersonalisationSms = new PaPayLaterListingPersonalisationSms(
-                paPayLaterListingTemplateId,
+        personalisation = new PaPayLaterListingPersonalisationSms(
+                templateId,
+                daysAfterNotificationSent,
                 iaAipFrontendUrl,
-                recipientsFinder
+                recipientsFinder,
+                systemDateProvider
         );
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(systemDateProvider.dueDate(daysAfterNotificationSent))
+                .thenReturn(mockedDueDate);
     }
 
     @Test
     void should_return_given_reference_id() {
-        assertEquals(caseId + "_PA_PAY_LATER_CASE_LISTING_SMS",
-                paPayLaterListingPersonalisationSms.getReferenceId(caseId));
+        assertEquals(
+                caseId + "_PA_PAY_LATER_LISTING_SMS",
+                personalisation.getReferenceId(caseId)
+        );
     }
 
     @Test
@@ -54,9 +75,12 @@ class PaPayLaterListingPersonalisationSmsTest {
         when(asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class))
                 .thenReturn(Optional.of(mockedAppealReferenceNumber));
 
-        Map<String, String> personalisation =
-                paPayLaterListingPersonalisationSms.getPersonalisation(asylumCase);
+        Map<String, String> personalisationMap =
+                personalisation.getPersonalisation(callback);
 
-        assertEquals(mockedAppealReferenceNumber, personalisation.get("appealReferenceNumber"));
+        assertEquals(
+                mockedAppealReferenceNumber,
+                personalisationMap.get("appealReferenceNumber")
+        );
     }
 }
