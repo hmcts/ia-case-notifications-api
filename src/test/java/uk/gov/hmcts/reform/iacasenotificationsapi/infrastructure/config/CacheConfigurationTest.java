6a5e1a6cf1fa1effdbe8c2670f4483bd07815b2b
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2026-03-11 17:09:43 +0000

    fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/CacheConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/CacheConfigurationTest.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.config;
+
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
