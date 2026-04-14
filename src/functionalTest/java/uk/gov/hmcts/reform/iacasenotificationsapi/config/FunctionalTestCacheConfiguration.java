871323a0a499b6cd466bff7f3a516000701460be
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2026-04-14 17:21:50 +0100

    Fix cache configuration for functional tests

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasenotificationsapi/config/FunctionalTestCacheConfiguration.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasenotificationsapi/config/FunctionalTestCacheConfiguration.java
@@ -1,12 +1,15 @@
 package uk.gov.hmcts.reform.iacasenotificationsapi.config;
 
-import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
@@ -15,7 +18,25 @@ public class FunctionalTestCacheConfiguration {
 
     @Bean
     @Primary
-    public CacheManagerCustomizer<CaffeineCacheManager> cacheManagerCustomizer() {
-        return cacheManager -> cacheManager.setAllowNullValues(false);
+    public CacheManager cacheManagerCustomizer() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager(
+                "userInfoCache",
+                "legalRepATokenCache",
+                "caseOfficerTokenCache",
+                "adminOfficerTokenCache",
+                "homeOfficeApcTokenCache",
+                "homeOfficeLartTokenCache",
+                "homeOfficePouTokenCache",
+                "homeOfficeGenericTokenCache",
+                "legalRepShareCaseATokenCache",
+                "judgeTokenCache",
+                "citizenTokenCache",
+                "systemTokenCache"
+        );
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(Duration.ofSeconds(3600))
+        );
+        cacheManager.setAllowNullValues(false);
+        return cacheManager;
     }
 }
