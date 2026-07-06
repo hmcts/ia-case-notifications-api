3e2095cbe60a8d33992cd7c9f98d4618a0204413
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2026-07-06 13:29:45 +0100

    DIAC-0000 updates to enable LR notification on LR manual appeal (#1684)
    * updates to enable LR notification on manual appeal
    
    * Update suppressions.xml
    
    ---------
    
    Co-authored-by: Rajesh Thuraiyur Moorthy <rajeshthuraiyurmoorthy@Mac.powerhub>

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -7207,8 +7207,8 @@ public class NotificationHandlerConfiguration {
 
                 return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                     && callback.getEvent() == Event.SEND_DIRECTION
-                    && isInternalNonStdDirectionWithParty(asylumCase, Parties.APPELLANT, directionFinder)
-                    && isInternalCase(asylumCase)
+                    && (isInternalNonStdDirectionWithParty(asylumCase, Parties.APPELLANT, directionFinder) ||
+                        isInternalNonStdDirectionWithParty(asylumCase, Parties.LEGAL_REPRESENTATIVE, directionFinder))
                     && (!isAppellantInDetention(asylumCase)
                     || (hasBeenSubmittedByAppellantInternalCase(asylumCase)
                     && isDetainedInFacilityType(asylumCase, OTHER))
