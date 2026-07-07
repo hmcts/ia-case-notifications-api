04c975c190168d43ad8485cebcb5a1c5b3c28d0f
Andrey Pereverzin <andreypereverzin@Mac.broadband>
2026-07-07 19:21:32 +0100

    DIAC-2302 CMR listing Send email notification to LR (Prison/IRC)

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/notificationhandlers/CmrNotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/notificationhandlers/CmrNotificationHandlerConfiguration.java
@@ -13,7 +13,6 @@ import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.NotificationGen
 
 import java.util.List;
 
-import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.APPEAL_REFERENCE_NUMBER;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.DetentionFacility.IRC;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.DetentionFacility.PRISON;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.utils.AsylumCaseUtils.*;
@@ -29,31 +28,6 @@ public class CmrNotificationHandlerConfiguration {
         return new NotificationHandler(
                 (callbackStage, callback) -> {
                     AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
-                    log.info("-----------------------------------------");
-                    log.info(
-                            "callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT: {}",
-                            callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
-                    );
-                    log.info(
-                            "Event.CMR_LISTING.equals(callback.getEvent()): {}",
-                            Event.CMR_LISTING.equals(callback.getEvent())
-                    );
-                    log.info(
-                            "isCmrHearingChannel(asylumCase, \"INTER\"): {}",
-                            isCmrHearingChannel(asylumCase, "INTER")
-                    );
-                    log.info(
-                            "isDetainedInOneOfFacilityTypes(asylumCase, IRC, PRISON): {}",
-                            isDetainedInOneOfFacilityTypes(asylumCase, IRC, PRISON)
-                    );
-                    log.info(
-                            "isRepJourney(callback.getCaseDetails().getCaseData()): {}",
-                            isRepJourney(callback.getCaseDetails().getCaseData())
-                    );
-                    log.info("case reference: {}", asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class).orElse(""));
-                    log.info("callback.getEvent(): {}", callback.getEvent());
-                    log.info("!isInternalCase(asylumCase): {}", !isInternalCase(asylumCase));
-                    log.info("-----------------------------------------");
                     return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                             && Event.CMR_LISTING.equals(callback.getEvent())
                             && isCmrHearingChannel(asylumCase, "INTER")
