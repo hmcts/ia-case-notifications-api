db8b16a0609f5a6a007d85dafe914be8508886b4
david-gimelle <david.gimelle@gmail.com>
2026-07-16 17:09:06 +0100

    [DIAC-2607] add integration test

--- src/integrationTest/java/uk/gov/hmcts/reform/iacasenotificationsapi/component/StatutoryTimeframe24WeeksNotificationsTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacasenotificationsapi/component/StatutoryTimeframe24WeeksNotificationsTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.iacasenotificationsapi.component;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -328,5 +329,16 @@ public class StatutoryTimeframe24WeeksNotificationsTest extends SpringBootIntegr
                                                                   List<String> expectedIds) {
         PreSubmitCallbackResponseForTest response = mockResponse(mockCaseData(testJourneyType, inCountry, wantsEmail, wantsSms, is24w), COMPLETE_CASE_REVIEW);
         assertNotificationsContain(response, expectedSize, expectedIds);
+    }
+
+    @Test
+    @WithMockUser(authorities = {"caseworker-ia-system"})
+    void should_send_24weeks_review_letter_to_legal_rep() {
+        var caseData = mockCaseData(TestJourneyType.LR, true, false, false, true);
+        var response = mockResponse(caseData, COMPLETE_CASE_REVIEW);
+        assertNotificationsContain(response, 3, List.of(
+            STATUTORY_TIMEFRAME_24WEEKS_CASE_REVIEW_LEGAL_REP_LETTER,
+            STATUTORY_TIMEFRAME_24WEEKS_CASE_REVIEW_LEGAL_REP_EMAIL,
+            STATUTORY_TIMEFRAME_24WEEKS_CASE_REVIEW_HOME_OFFICE_EMAIL));
     }
 }
