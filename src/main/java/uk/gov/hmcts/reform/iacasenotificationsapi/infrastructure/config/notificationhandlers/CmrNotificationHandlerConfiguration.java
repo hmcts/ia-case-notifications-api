65d5725e8ed051004a83ff2ec5c8fd064a84e02e
tiagreaney <tiagreaney@gmail.com>
2026-07-28 08:35:48 +0100

    update handler

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/notificationhandlers/CmrNotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/notificationhandlers/CmrNotificationHandlerConfiguration.java
@@ -234,12 +234,12 @@ public class CmrNotificationHandlerConfiguration {
         return new NotificationHandler(
                 (callbackStage, callback) -> {
                     AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
-                    log.info("isAipJourney: {} for case reference: {}", isAipJourney(callback.getCaseDetails().getCaseData()),  callback.getCaseDetails().getId());
-                    log.info("isInternalCase: {} for case reference: {}", isInternalCase(callback.getCaseDetails().getCaseData()), callback.getCaseDetails().getId());
+                    log.info("isCmrHearingInPersonOrRemote : {} for case reference: {}", isCmrHearingInPersonOrRemote(asylumCase),  callback.getCaseDetails().getId());
+                    log.info("hasBeenSubmittedByAppellantInternalCase: {} for case reference: {}", hasBeenSubmittedByAppellantInternalCase(asylumCase), callback.getCaseDetails().getId());
                     return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                             && CMR_HEARING_CANCELLED.equals(callback.getEvent())
-                            && isAipJourney(callback.getCaseDetails().getCaseData())
-                            && isInternalCase(asylumCase);
+                            && isCmrHearingInPersonOrRemote(asylumCase)
+                            && hasBeenSubmittedByAppellantInternalCase(asylumCase);
                 },
                 notificationGenerators
         );
