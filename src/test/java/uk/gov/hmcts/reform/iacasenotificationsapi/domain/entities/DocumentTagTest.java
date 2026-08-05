24700ff151f2abad77346dc499e77261544f01ce
Andrey Pereverzin <andreypereverzin@Mac.broadband>
2026-08-05 17:12:43 +0100

    DIAC-2305

--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/DocumentTagTest.java
@@ -113,6 +113,6 @@ class DocumentTagTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(109, DocumentTag.values().length);
+        assertEquals(107, DocumentTag.values().length);
     }
 }
