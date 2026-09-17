5c65d19624d0442c3a192c05553fee5594a5775d
david-gimelle <david.gimelle@gmail.com>
2026-09-17 13:23:57 +0100

    [2713] add guard to not send double template when listed first time

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/infrastructure/config/NotificationHandlerConfiguration.java
@@ -63,7 +63,6 @@ import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AppealT
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AppealType.PA;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ApplicantType.APPELLANT;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.APPEAL_TYPE;
-import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.ARIA_LISTING_REFERENCE;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.CONTACT_PREFERENCE;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.DECIDE_AN_APPLICATION_ID;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.DETENTION_FACILITY;
@@ -193,6 +192,7 @@ import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.utils.AsylumCase
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.utils.AsylumCaseUtils.isHearingChannel;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.utils.AsylumCaseUtils.isHearingDetailsUpdated;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.utils.AsylumCaseUtils.hasStf24WeeksStatus;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.utils.AsylumCaseUtils;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.utils.AsylumCaseUtils.isInternalCase;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.utils.AsylumCaseUtils.isLegalRepEjp;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.utils.AsylumCaseUtils.isRemissionApproved;
@@ -7993,17 +7993,16 @@ public class NotificationHandlerConfiguration {
             (callbackStage, callback) -> {
                 final AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
                 boolean isStf24W = hasStf24WeeksStatus(asylumCase);
-                boolean isRelisting = callback.getCaseDetailsBefore()
+                boolean relisting = callback.getCaseDetailsBefore()
                     .map(CaseDetails::getCaseData)
-                    .flatMap(before -> before.read(ARIA_LISTING_REFERENCE, String.class))
-                    .filter(ref -> !ref.isBlank())
-                    .isPresent();
+                    .map(AsylumCaseUtils::isRelisting)
+                    .orElse(false);
                 log.debug("reListCase24WeeksNotificationHandler canHandle: event={}, stage={}, isStf24W={}, isRelisting={}, caseId={}",
-                    callback.getEvent(), callbackStage, isStf24W, isRelisting, callback.getCaseDetails().getId());
+                    callback.getEvent(), callbackStage, isStf24W, relisting, callback.getCaseDetails().getId());
                 return callback.getEvent() == LIST_CASE
                     && callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                     && isStf24W
-                    && isRelisting;
+                    && relisting;
             }, notificationGenerators
         );
     }
